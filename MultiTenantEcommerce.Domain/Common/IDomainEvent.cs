a1d6f7b66495d29ba715b98cca92acad2fdd86d4
david <davidrijo18@gmail.com>
2025-08-15 03:24:27 +0100

    Created support for event-driven architecture

---
+++ MultiTenantEcommerce.Domain/Common/IDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace MultiTenantEcommerce.Domain.Common;
+
+//Interface para os eventos, herda o INotification do MediatR
+
+public interface IDomainEvent : INotification
+{
+    DateTime OccurredOn { get; }
+}
---
+++ MultiTenantEcommerce.Domain/Common/IHasDomainEvents.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiTenantEcommerce.Domain.Common;
+
+//Interface para as entities poderem guardar eventos
+
+public interface IHasDomainEvents
+{
+    List<IDomainEvent> DomainEvents { get; }
+    void ClearDomainEvents();
+}
