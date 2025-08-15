d4fcaaecdff79a7e3989e8b609557216d98b1bf6
david <davidrijo18@gmail.com>
2025-08-15 05:12:30 +0100

    Small fix verify same email

--- MultiTenantEcommerce.Domain/ValueObjects/Email.cs
+++ MultiTenantEcommerce.Domain/ValueObjects/Email.cs
@@ -4,7 +4,7 @@ namespace MultiTenantEcommerce.Domain.ValueObjects;
 public class Email
 {
     public string Value { get; private set; }
-    const string Pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+    private const string Pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
 
     private Email() { }
     public Email(string email)
@@ -30,21 +30,7 @@ public class Email
 
     private void VerifySameEmail(string email)
     {
-        if(email == Value)
-            throw new Exception("New email can't be equal to previous email.");
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is null) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != GetType()) return false;
-        var other = (Email)obj;
-        return Value == other.Value;
-    }
-
-    public override int GetHashCode()
-    {
-        return Value.GetHashCode();
+        if (email.Equals(Value, StringComparison.OrdinalIgnoreCase))
+            throw new InvalidOperationException("New email can't be equal to previous email.");
     }
 }
