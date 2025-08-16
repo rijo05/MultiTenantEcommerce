bc3b0b99fc6968958f3fd3299adcd84239ccf7bc
david <davidrijo18@gmail.com>
2025-08-16 02:55:18 +0100

    Refactor: Rename field and modify Order constructor

--- MultiTenantEcommerce.Domain/Entities/Order.cs
+++ MultiTenantEcommerce.Domain/Entities/Order.cs
@@ -10,32 +10,33 @@ public class Order : IHasDomainEvents
     public Guid TenantId { get; private set; }
     public Guid CustomerId { get; private set; }
     public OrderStatus OrderStatus { get; private set; }
+    public PaymentMethod PaymentMethod { get; private set; }
     public DateTime CreatedAt { get; private set; }
-    public DateTime LastUpdatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
     public DateTime? PayedAt { get; private set; }
     public Address Address { get; private set; }
-
-    private readonly List<OrderItem> _items = new();
+    public decimal Price => _items.Sum(x => x.Total);
     public IReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
+    private readonly List<OrderItem> _items = new();
 
+    #region domain events
     private readonly List<IDomainEvent> _domainEvents = new();
     public List<IDomainEvent> DomainEvents => _domainEvents;
     public void ClearDomainEvents() => _domainEvents.Clear();
-
-    public decimal TotalPrice => _items.Sum(x => x.Total);
+    #endregion
 
     private Order() { }
-
-    public Order(Guid OrderId, Guid tenantId, Guid customerId, Address address, IEnumerable<OrderItem> items)
+    public Order(Guid tenantId, Guid customerId, Address address, IEnumerable<OrderItem> items, PaymentMethod paymentMethod)
     {
-        Id = OrderId;
+        Id = Guid.NewGuid();
         TenantId = tenantId;
         CustomerId = customerId;
         OrderStatus = OrderStatus.PendingPayment;
         CreatedAt = DateTime.UtcNow;
-        LastUpdatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
         Address = address;
         _items.AddRange(items);
+        PaymentMethod = paymentMethod;
     }
 
     #region CHANGE ORDER STATUS
@@ -44,11 +45,13 @@ public class Order : IHasDomainEvents
     {
         OrderStatusTransitionValidator.ValidateTransition(this.OrderStatus, newStatus);
         OrderStatus = newStatus;
-        LastUpdatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
 
         TriggerDomainEvents(newStatus);
     }
+    #endregion
 
+    #region PRIVATES
     private void TriggerDomainEvents(OrderStatus newStatus)
     {
         switch (newStatus)
