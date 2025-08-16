22e6add36a245c82a48a497803b5c3c1c1ab9c68
david <davidrijo18@gmail.com>
2025-08-16 03:17:05 +0100

    Feat: Create StockRepository and its interface

---
+++ MultiTenantEcommerce.Domain/Interfaces/IStockRepository.cs
@@ -0,0 +1,8 @@
+﻿using MultiTenantEcommerce.Domain.Entities;
+
+namespace MultiTenantEcommerce.Domain.Interfaces;
+public interface IStockRepository : IRepository<Stock>
+{
+    public Task<Stock?> GetByProductId(Guid productId);
+    public Task AddBulkAsync(List<Stock> items);
+}
---
+++ MultiTenantEcommerce.Infrastructure/Repositories/StockRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiTenantEcommerce.Domain.Entities;
+using MultiTenantEcommerce.Domain.Interfaces;
+
+namespace MultiTenantEcommerce.Infrastructure.Repositories;
+public class StockRepository : Repository<Stock>, IStockRepository
+{
+    public StockRepository(AppDbContext appDbContext) : base(appDbContext) { }
+
+    public async Task<Stock?> GetByProductId(Guid productId)
+    {
+        return await _appDbContext.Stocks.FirstOrDefaultAsync(x => x.ProductId == productId);
+    }
+
+    public async Task AddBulkAsync(List<Stock> items)
+    {
+        await _appDbContext.Stocks.AddRangeAsync(items);
+    }
+}
