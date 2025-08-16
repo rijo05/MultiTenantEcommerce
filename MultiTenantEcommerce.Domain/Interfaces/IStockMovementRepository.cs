ef625c67d96fba41747039cd2d7475de03f2db4d
david <davidrijo18@gmail.com>
2025-08-16 03:16:38 +0100

    Feat: Create StockMovementRepository and its interface

---
+++ MultiTenantEcommerce.Domain/Interfaces/IStockMovementRepository.cs
@@ -0,0 +1,17 @@
+﻿using MultiTenantEcommerce.Domain.Entities;
+using MultiTenantEcommerce.Domain.Enums;
+
+namespace MultiTenantEcommerce.Domain.Interfaces;
+public interface IStockMovementRepository : IRepository<StockMovement>
+{
+    public Task<List<StockMovement>> GetFilteredAsync(
+    Guid? productId = null,
+    int? quantity = null,
+    DateTime? minDate = null,
+    DateTime? maxDate = null,
+    string? reason = null,
+    int page = 1,
+    int pageSize = 20,
+    SortOptions? sort = null);
+}
+
