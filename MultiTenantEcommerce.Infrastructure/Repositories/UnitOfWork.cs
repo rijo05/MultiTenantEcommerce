d862577b7e4312059ea64c725dcc990db93ef915
david <davidrijo18@gmail.com>
2025-08-16 03:19:05 +0100

    Feat: Create UnitOfWork

---
+++ MultiTenantEcommerce.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using MultiTenantEcommerce.Domain.Interfaces;
+
+namespace MultiTenantEcommerce.Infrastructure.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    private readonly IMediator _mediator;
+
+    public UnitOfWork(AppDbContext context, IMediator mediator)
+    {
+        _context = context;
+        _mediator = mediator;
+    }
+
+    public async Task<int> CommitAsync()
+    {
+        var result = await _context.SaveChangesAsync();
+
+        var events = _context.GetAllDomainEvents();
+        _context.ClearAllDomainEvents();
+
+        foreach (var domainEvent in events)
+            await _mediator.Publish(domainEvent);
+
+        return result;
+    }
+}
