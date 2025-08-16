18a07a8d75ea5422fbb6e92c02ac254a327ab595
david <davidrijo18@gmail.com>
2025-08-16 03:13:45 +0100

    Feat: Create EmployeeRepository and its interface

---
+++ MultiTenantEcommerce.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiTenantEcommerce.Domain.Entities;
+using MultiTenantEcommerce.Domain.Enums;
+using MultiTenantEcommerce.Domain.Interfaces;
+using MultiTenantEcommerce.Domain.ValueObjects;
+
+namespace MultiTenantEcommerce.Infrastructure.Repositories;
+public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+{
+    public EmployeeRepository(AppDbContext appDbContext) : base(appDbContext) { }
+            
+    public async Task<Employee?> GetByEmailAsync(Email email)
+    {
+        return await _appDbContext
+                        .Employees
+                        .FirstOrDefaultAsync(u => u.Email.Value == email.Value);
+    }
+
+    public async Task<List<Employee>> GetByNameAsync(string name)
+    {
+        return await _appDbContext.Employees
+            .Where(u => EF.Functions.Like(u.Name, $"%{name}%"))
+            .ToListAsync();
+    }
+
+    public async Task<List<Employee>> GetFilteredAsync(
+    string? name = null,
+    string? role = null,
+    string? email = null,
+    bool? isActive = null,
+    int page = 1,
+    int pageSize = 20,
+    SortOptions? sort = null)
+    {
+        var query = _appDbContext.Employees.AsQueryable();
+
+        if (!string.IsNullOrWhiteSpace(name))
+            query = query.Where(p => EF.Functions.Like(p.Name, $"%{name}%"));
+
+        if (isActive.HasValue)
+            query = query.Where(p => p.IsActive == isActive.Value);
+
+        if (!string.IsNullOrWhiteSpace(email))
+            query = query.Where(p => p.Email != null && EF.Functions.Like(p.Email.Value, $"%{email}%"));
+
+        if (!string.IsNullOrWhiteSpace(role))
+            query = query.Where(p => p.Role != null && EF.Functions.Like(p.Role.roleName.ToString(), $"%{role}%"));
+
+
+        return await SortAndPageAsync(query, sort, page, pageSize);
+    }
+}
