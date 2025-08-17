2dae383356144bd7d931015d0bcbcc1fe6e685b3
david <davidrijo18@gmail.com>
2025-08-17 16:28:11 +0100

    Feat: create AppSettings class to hold platform configuration

---
+++ MultiTenantEcommerce.Infrastructure/Configurations/AppSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace MultiTenantEcommerce.Infrastructure.Configurations;
+public class AppSettings
+{
+    public string PlatformName { get; set; }
+    public string PlatformUrl { get; set; }
+}
