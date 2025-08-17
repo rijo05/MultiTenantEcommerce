9b5cb84cf7ea311c8a9e884aa964faad7be1981e
david <davidrijo18@gmail.com>
2025-08-17 15:16:23 +0100

    feat: implement TenantMiddleware to retrieve tenant ID from URL

---
+++ MultiTenantEcommerce.API/Middleware/TenantMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Options;
+using MultiTenantEcommerce.Domain.Interfaces;
+using MultiTenantEcommerce.Infrastructure.Configurations;
+using MultiTenantEcommerce.Infrastructure.Context;
+
+namespace MultiTenantEcommerce.API.Middleware;
+
+public class TenantMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ITenantRepository _tenantRepository ;
+
+    public TenantMiddleware(RequestDelegate request, ITenantRepository tenantRepository)
+    {
+        _next = request;
+        _tenantRepository = tenantRepository;
+    }
+
+    public async Task InvokeAsync(HttpContext context, TenantContext tenantContext, IOptions<AppSettings> appSettings)
+    {
+        var companyName = context.Request.Host.Value.Replace("."+ appSettings.Value.PlatformUrl,"");
+
+        var tenant = await _tenantRepository.GetByCompanyName(companyName);
+
+        if (tenant == null)
+            throw new Exception("That store doesnt exist");
+
+        tenantContext.TenantId = tenant.Id;
+
+        await _next(context);
+    }
+}
