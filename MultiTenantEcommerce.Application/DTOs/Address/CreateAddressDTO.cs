771a88bf1660eb391f76a25c5e81360c3ed5e38b
david <didrijo@gmail.com>
2025-08-18 17:09:32 +0100

    Feat: Create DTOs for Address

---
+++ MultiTenantEcommerce.Application/DTOs/Address/CreateAddressDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiTenantEcommerce.Application.DTOs.Address;
+
+public class CreateAddressDTO
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string HouseNumber { get; set; }
+}
