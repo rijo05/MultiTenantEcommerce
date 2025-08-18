452f1bcba65f4b2f86db0d4082cf77cc7384fc46
david <didrijo@gmail.com>
2025-08-18 17:10:22 +0100

    Feat: Create DTOs for OrderItem

---
+++ MultiTenantEcommerce.Application/DTOs/OrderItems/CreateOrderItemDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiTenantEcommerce.Application.DTOs.OrderItems;
+
+public class CreateOrderItemDTO
+{
+    public Guid Id { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ MultiTenantEcommerce.Application/DTOs/OrderItems/OrderItemResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiTenantEcommerce.Application.DTOs.OrderItems;
+
+public class OrderItemResponseDTO
+{
+    public Guid OrderId { get; init; }
+    public Guid ProductId { get; init; }
+    public string Name { get; init; }
+    public decimal UnitPrice { get; init; }
+    public int Quantity { get; init; }
+    public decimal Total => Quantity * UnitPrice;
+}
