f0ba77c4b430a6433a638440f649f985fe37b604
david <didrijo@gmail.com>
2025-08-21 02:10:45 +0100

    Feat: implement GetFilteredAsync for tenant

--- MultiTenantEcommerce.Domain/Interfaces/ITenantRepository.cs
+++ MultiTenantEcommerce.Domain/Interfaces/ITenantRepository.cs
@@ -1,7 +1,13 @@
 ﻿using MultiTenantEcommerce.Domain.Entities;
+using MultiTenantEcommerce.Domain.Enums;
 
 namespace MultiTenantEcommerce.Domain.Interfaces;
 public interface ITenantRepository : IRepository<Tenant>
 {
     public Task<Tenant?> GetByCompanyName(string companyName);
+
+    public Task<List<Tenant>> GetFilteredAsync(
+        int page = 1,
+        int pageSize = 20,
+        SortOptions? sort = null);
 }
