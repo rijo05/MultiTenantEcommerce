6cc8ec2f6044fd8bb91c3d160b453f234d361f59
david <didrijo@gmail.com>
2025-08-21 02:07:55 +0100

    Feat: Create Tenant DTOs validators

---
+++ MultiTenantEcommerce.Application/Validators/TenantValidator/CreateTenantDTOValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using MultiTenantEcommerce.Application.DTOs.Tenant;
+using MultiTenantEcommerce.Application.Validators.Common;
+
+namespace MultiTenantEcommerce.Application.Validators.TenantValidator;
+public class CreateTenantDTOValidator : AbstractValidator<CreateTenantDTO>
+{
+    public CreateTenantDTOValidator()
+    {
+        RuleFor(x => x.CompanyName).UserNameRules();
+    }
+}
---
+++ MultiTenantEcommerce.Application/Validators/TenantValidator/UpdateTenantDTOValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using MultiTenantEcommerce.Application.DTOs.Tenant;
+using MultiTenantEcommerce.Application.Validators.Common;
+
+namespace MultiTenantEcommerce.Application.Validators.TenantValidator;
+public class UpdateTenantDTOValidator : AbstractValidator<UpdateTenantDTO>
+{
+    public UpdateTenantDTOValidator()
+    {
+        RuleFor(x => x.CompanyName).NameRules()
+            .When(x => !string.IsNullOrEmpty(x.CompanyName));
+    }
+}
