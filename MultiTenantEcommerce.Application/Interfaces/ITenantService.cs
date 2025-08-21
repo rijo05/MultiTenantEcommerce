3dc88cb00b7f26a788c5094aa7533ea55cee67da
david <didrijo@gmail.com>
2025-08-21 02:00:42 +0100

    Feat: create TenantService and its interface

---
+++ MultiTenantEcommerce.Application/Interfaces/ITenantService.cs
@@ -0,0 +1,13 @@
+﻿using MultiTenantEcommerce.Application.DTOs.Product;
+using MultiTenantEcommerce.Application.DTOs.Tenant;
+
+namespace MultiTenantEcommerce.Application.Interfaces;
+public interface ITenantService
+{
+    public Task<TenantResponseDTO?> GetTenantByIdAsync(Guid id);
+    public Task<List<TenantResponseDTO>> GetFilteredTenantsAsync(TenantFilterDTO tenantFilterDTO);
+
+    public Task<TenantResponseDTO> AddTenantAsync(CreateTenantDTO tenant);
+    public Task DeleteTenantAsync(Guid id);
+    public Task<TenantResponseDTO> UpdateTenantAsync(Guid id, UpdateTenantDTO updatedTenant);
+}
