70f5134a6c14241f61c106e3d1565b2852a888d9
david <didrijo@gmail.com>
2025-08-27 23:39:31 +0100

    Feat: add DTOs for customer

---
+++ MultiTenantEcommerce.Application/DTOs/Customer/CustomerResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using MultiTenantEcommerce.Application.DTOs.Address;
+
+namespace MultiTenantEcommerce.Application.DTOs.Customer;
+public class CustomerResponseDTO
+{
+    public Guid Id { get; init; }
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string PhoneNumber { get; init; }
+    public string CountryCode { get; init; }
+    public AddressResponseDTO Address { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; init; }
+}
