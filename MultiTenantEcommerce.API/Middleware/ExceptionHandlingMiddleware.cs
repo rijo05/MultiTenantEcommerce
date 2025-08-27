463fd875e0ca81c7005b61a036a013934c09f66c
david <didrijo@gmail.com>
2025-08-27 23:39:54 +0100

    Feat: implement exception handling middleware

---
+++ MultiTenantEcommerce.API/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+
+namespace MultiTenantEcommerce.API.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _requestDelegate;
+
+    public ExceptionHandlingMiddleware(RequestDelegate requestDelegate)
+    {
+        _requestDelegate = requestDelegate;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _requestDelegate(context);
+        }
+        catch (ValidationException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Response.ContentType = "application/json";
+
+            var erros = ex.Errors.Select(e => new
+            {
+                field = e.PropertyName,
+                message = e.ErrorMessage
+            });
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                error = "Validation Failed",
+                errors = erros
+            });
+        }
+        catch(Exception ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            context.Response.ContentType = "application/json";
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                error = "Internal Server Error",
+                errors = ex.Message
+            });
+        }
+    }
+}
