23bdf3a5d781f099d5d22acf2b1836f1e1e9495c
david <didrijo@gmail.com>
2025-08-31 21:17:18 +0100

    Feat: add generic ICommand and IQuery interfaces and its handlers

---
+++ MultiTenantEcommerce.Application/Common/Interfaces/ICommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace MultiTenantEcommerce.Application.Common.Interfaces;
+public interface ICommand<TResponse> : IRequest<TResponse> { }
+
+public interface ICommandHandler<TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>{ }
---
+++ MultiTenantEcommerce.Application/Common/Interfaces/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace MultiTenantEcommerce.Application.Common.Interfaces;
+public interface IQuery<TResponse> : IRequest<TResponse> { }
+
+public interface IQueryHandler<TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>{ }
