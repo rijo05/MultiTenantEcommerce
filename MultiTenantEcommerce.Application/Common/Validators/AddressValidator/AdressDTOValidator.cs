6f49dfbd222ef1542fc48c1ad21bd279e21afbd7
david <didrijo@gmail.com>
2025-08-31 21:19:06 +0100

    Refactor: move address-related code to Common folder

---
+++ MultiTenantEcommerce.Application/Common/Validators/AddressValidator/AdressDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MultiTenantEcommerce.Application.Common.DTOs.Address;
+using MultiTenantEcommerce.Application.Common.Validators;
+
+namespace MultiTenantEcommerce.Application.Common.Validators.AddressValidator;
+
+public class AddressDTOValidator : AbstractValidator<CreateAddressDTO>
+{
+    public AddressDTOValidator()
+    {
+        RuleFor(x => x.Street).StreetRules();
+
+        RuleFor(x => x.City).CityRules();
+
+        RuleFor(x => x.PostalCode).PostalCodeRules();
+
+        RuleFor(x => x.Country).CountryRules();
+
+        RuleFor(x => x.HouseNumber).HouseNumberRules();
+    }
+}
