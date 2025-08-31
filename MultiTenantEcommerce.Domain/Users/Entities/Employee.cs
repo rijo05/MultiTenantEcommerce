b899dcf5bfc9f6ccacc88319ebd092ce10372122
david <didrijo@gmail.com>
2025-08-31 20:19:38 +0100

    Feat: implement logic to update employee

--- MultiTenantEcommerce.Domain/Users/Entities/Employee.cs
+++ MultiTenantEcommerce.Domain/Users/Entities/Employee.cs
@@ -8,7 +8,8 @@ public class Employee : UserBase
     public Password Password { get; private set; }
 
     private Employee() { }
-    public Employee(Guid tenantId, string name, Email email, Role role, Password password) : base(tenantId, name, email)
+    public Employee(Guid tenantId, string name, Email email, Role role, Password password) 
+        : base(tenantId, name, email)
     {
         Role = role;
         Password = password;
@@ -16,6 +17,28 @@ public class Employee : UserBase
 
     #region UPDATE DATA
 
+    public void UpdateEmployee(
+        string? name,
+        string? email,
+        string? password,
+        string? role,
+        bool? isActive)
+    {
+        if (!string.IsNullOrWhiteSpace(name))
+            UpdateName(name);
+
+        if (!string.IsNullOrWhiteSpace(email))
+            UpdateEmail(email);
+
+        if (!string.IsNullOrWhiteSpace(password))
+            UpdatePassword(password);
+
+        if (isActive.HasValue)
+            SetActive(isActive.Value);
+
+        if (!string.IsNullOrWhiteSpace(role))
+            UpdateRole(role);
+    }
     public void UpdatePassword(string newPassword)
     {
         Password.UpdatePassword(newPassword);
