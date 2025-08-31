de52ba087c267a100e522e89b26f07bc33153d27
david <didrijo@gmail.com>
2025-08-31 20:19:12 +0100

    Feat: implement logic to update customer

--- MultiTenantEcommerce.Domain/Users/Entities/Customer.cs
+++ MultiTenantEcommerce.Domain/Users/Entities/Customer.cs
@@ -1,4 +1,6 @@
-﻿using MultiTenantEcommerce.Domain.ValueObjects;
+﻿using MediatR;
+using MultiTenantEcommerce.Domain.Users.Interfaces;
+using MultiTenantEcommerce.Domain.ValueObjects;
 
 namespace MultiTenantEcommerce.Domain.Users.Entities;
 public class Customer : UserBase
@@ -8,22 +10,55 @@ public class Customer : UserBase
     public PhoneNumber PhoneNumber { get; private set; }
 
     private Customer() { }
-    public Customer(Guid tenantId, string name, Email email, Password password, Address address, PhoneNumber phoneNumber) : base(tenantId, name, email)
+    public Customer(Guid tenantId, string name, Email email, Password password, Address address, PhoneNumber phoneNumber) 
+        : base(tenantId, name, email)
     {
         Address = address;
         Password = password;
         PhoneNumber = phoneNumber;
     }
-
-    public void UpdatePassword(string newPassword)
+    public void UpdateCustomer(
+        string? name,
+        string? email,
+        string? password
+        /*string? phoneNumber,
+        //string? countryCode,
+        //string? address = null*/)
     {
-        Password.UpdatePassword(newPassword);
+        if (!string.IsNullOrWhiteSpace(name))
+            UpdateName(name);
+
+        if (email != null)
+            UpdateEmail(email);
+
+        if (password != null)
+            UpdatePassword(password);
+
+        //if (phoneNumber != null)
+        //    UpdatePhoneNumber(phoneNumber);
+
+        //if (address != null)
+        //    UpdateAddress(address);
+
+        //TODO() ver como atualizar address e phone number ##########
+    }
+
+
+    public void UpdatePassword(string? password)
+    {
+        Password.UpdatePassword(password);
         SetUpdatedAt();
     }
 
-    public void UpdateAddress(string address)
-    {
-        //TODO()
-        SetUpdatedAt();
-    }
+    //private void UpdatePhoneNumber(PhoneNumber phoneNumber)
+    //{
+    //    if (phoneNumber == null) throw new Exception("Phone number cannot be null.");
+    //    PhoneNumber = phoneNumber;
+    //}
+
+    //private void UpdateAddress(Address address)
+    //{
+    //    if (address == null) throw new Exception("Address cannot be null.");
+    //    Address = address;
+    //}
 }
