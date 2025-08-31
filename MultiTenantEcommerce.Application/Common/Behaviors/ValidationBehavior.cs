889a9aea2ae185bfbbdec53ee63a32726a5f7dae
david <didrijo@gmail.com>
2025-08-31 21:15:58 +0100

    Feat: add ValidationBehavior pipeline for request validation

---
+++ MultiTenantEcommerce.Application/Common/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MediatR;
+using MultiTenantEcommerce.Application.Common.Validators;
+
+namespace MultiTenantEcommerce.Application.Common.Behaviors;
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IValidator<TRequest> _validator;
+
+    public ValidationBehavior(IValidator<TRequest> validator)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        await ValidationRules.ValidateAsync(request, _validator);
+
+        return await next();
+    }
+}
