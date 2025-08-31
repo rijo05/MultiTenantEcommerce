394b3d2cb4990881747ee5635dffda57d66cdfd4
david <didrijo@gmail.com>
2025-08-31 20:14:46 +0100

    Feat: implement logic to update product and removed SKU field

--- MultiTenantEcommerce.Domain/Catalog/Entities/Product.cs
+++ MultiTenantEcommerce.Domain/Catalog/Entities/Product.cs
@@ -1,4 +1,6 @@
-﻿using MultiTenantEcommerce.Domain.Common.Entities;
+﻿using MediatR;
+using MultiTenantEcommerce.Domain.Catalog.Interfaces;
+using MultiTenantEcommerce.Domain.Common.Entities;
 using MultiTenantEcommerce.Domain.Common.Guard;
 using MultiTenantEcommerce.Domain.ValueObjects;
 
@@ -7,13 +9,13 @@ public class Product : TenantBase
 {
     public string Name { get; private set; }
     public string? Description { get; private set; }
-    public string? SKU { get; private set; }
     public Money Price { get; private set; }
     public bool IsActive { get; private set; }
     public Guid CategoryId { get; private set; }
 
     private Product() { }
-    public Product(Guid tenantId, string name, Money price, Guid categoryId, string? sku, string? description, bool isActive = true) : base(tenantId)
+    public Product(Guid tenantId, string name, Money price, Guid categoryId, string? description, bool isActive = true) 
+        : base(tenantId)
     {
         GuardCommon.AgainstNullOrEmpty(name, nameof(name));
         GuardCommon.AgainstMaxLength(name, 100, nameof(name));
@@ -22,13 +24,40 @@ public class Product : TenantBase
 
         Name = name;
         Description = description;
-        SKU = sku;
         Price = price;
         IsActive = isActive;
         CategoryId = categoryId;
     }
 
     #region UPDATE DATA
+
+    public void UpdateProduct(string? Name,
+    string? Description,
+    decimal? Price,
+    bool? IsActive,
+    Guid? CategoryId)
+    {
+        if (CategoryId.HasValue)
+            UpdateCategory(CategoryId.Value);
+
+
+        if (!string.IsNullOrEmpty(Name))
+            UpdateName(Name);
+
+        if (Description is not null)
+        {
+            if (Description == "")
+                ClearDescription();
+            else
+                UpdateDescription(Description);
+        }
+
+        if (Price.HasValue)
+            ChangePrice(Price.Value);
+
+        if (IsActive.HasValue)
+            ChangeActive(IsActive.Value);
+    }
     public void UpdateName(string newName)
     {
         GuardCommon.AgainstNullOrEmpty(newName, nameof(newName));
@@ -60,12 +89,6 @@ public class Product : TenantBase
     public void ChangeActive(bool isActive)
     {
         IsActive = isActive;
-        SetUpdatedAt();
-    }
-    public void ChangeSKU(string sku)
-    {
-        GuardCommon.AgainstNullOrEmpty(sku, nameof(sku));
-        SKU = sku;
         SetUpdatedAt();
     }
 
