4abbfd2c038e4183809db247c16a4ff086f44b33
david <didrijo@gmail.com>
2025-09-08 23:08:12 +0100

    Feat: create payment configuration

---
+++ MultiTenantEcommerce.Infrastructure/Persistence/Configurations/PaymentConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MultiTenantEcommerce.Domain.Payment.Entities;
+using MultiTenantEcommerce.Domain.Tenants.Entities;
+using MultiTenantEcommerce.Infrastructure.Persistence.Context;
+
+namespace MultiTenantEcommerce.Infrastructure.Persistence.Configurations;
+public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+{
+    private readonly TenantContext _tenantContext;
+
+    public PaymentConfiguration(TenantContext tenantContext)
+    {
+        _tenantContext = tenantContext;
+    }
+
+    public void Configure(EntityTypeBuilder<Payment> builder)
+    {
+        builder.HasKey(x => new { x.TenantId, x.Id });
+
+        builder.HasOne<Tenant>()
+                .WithMany()
+                .HasForeignKey(x => x.TenantId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        builder.OwnsOne(u => u.Amount, amount =>
+        {
+            amount.Property(e => e.Value)
+                .HasColumnName("Amount")
+                .IsRequired();
+        });
+
+        builder.Property(x => x.Reason)
+                .HasConversion<string>()
+                .IsRequired();
+
+        builder.Property(x => x.Status)
+                .HasConversion<string>()
+                .IsRequired();
+
+        builder.Property(x => x.PaymentMethod)
+               .HasConversion<string>()
+               .IsRequired();
+
+        builder.HasQueryFilter(x => x.TenantId == _tenantContext.TenantId);
+    }
+}
