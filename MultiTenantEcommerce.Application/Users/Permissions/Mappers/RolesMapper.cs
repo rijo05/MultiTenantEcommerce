2fc2d402d4b00ff9c15bd32b106691cb2a1cdb03
david <didrijo@gmail.com>
2025-09-08 22:19:18 +0100

    feat: create roleMapper

---
+++ MultiTenantEcommerce.Application/Users/Permissions/Mappers/RolesMapper.cs
@@ -0,0 +1,42 @@
+﻿using MultiTenantEcommerce.Application.Users.Permissions.DTOs;
+using MultiTenantEcommerce.Domain.Users.Entities.Permissions;
+
+namespace MultiTenantEcommerce.Application.Users.Permissions.Mappers;
+public class RolesMapper
+{
+
+    public RoleResponseDTO ToRoleResponseDTO(Role role)
+    {
+        return new RoleResponseDTO()
+        {
+            Id = role.Id,
+            Name = role.Name,
+            Description = role.Description,
+            Permissions = ToPermissionResponseDTOList(role.Permissions.ToList()),
+            CreatedAt = role.CreatedAt,
+            UpdatedAt = role.UpdatedAt,
+        };
+    }
+
+    public List<RoleResponseDTO> ToRoleResponseDTOList(IEnumerable<Role> roles)
+    {
+        return roles.Select(x => ToRoleResponseDTO(x)).ToList();
+    }
+
+    public PermissionResponseDTO ToPermissionResponseDTO(Permission permission)
+    {
+        return new PermissionResponseDTO()
+        {
+            Id = permission.Id,
+            Name = permission.Name,
+            Description = permission.Description,
+            Action = permission.Action,
+            Area = permission.Area,
+        };
+    }
+
+    public List<PermissionResponseDTO> ToPermissionResponseDTOList(IEnumerable<Permission> permissions)
+    {
+        return permissions.Select(x => ToPermissionResponseDTO(x)).ToList();
+    }
+}
