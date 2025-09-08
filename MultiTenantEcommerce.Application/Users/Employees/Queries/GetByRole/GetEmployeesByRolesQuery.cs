51e76ef752c67a74fedecd5e85bf4f5cce108168
david <didrijo@gmail.com>
2025-09-08 22:12:50 +0100

    Feat: create GetEmployeesByRolesQuery and handle

---
+++ MultiTenantEcommerce.Application/Users/Employees/Queries/GetByRole/GetEmployeesByRolesQuery.cs
@@ -0,0 +1,6 @@
+﻿using MultiTenantEcommerce.Application.Common.Interfaces.CQRS;
+using MultiTenantEcommerce.Application.Users.Employees.DTOs;
+
+namespace MultiTenantEcommerce.Application.Users.Employees.Queries.GetByRole;
+public record GetEmployeesByRolesQuery(
+    Guid roleId) : IQuery<List<EmployeeResponseDTO>>;
---
+++ MultiTenantEcommerce.Application/Users/Employees/Queries/GetByRole/GetEmployeesByRolesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MultiTenantEcommerce.Application.Common.Interfaces.CQRS;
+using MultiTenantEcommerce.Application.Users.Employees.DTOs;
+using MultiTenantEcommerce.Application.Users.Employees.Mappers;
+using MultiTenantEcommerce.Domain.Users.Interfaces;
+
+namespace MultiTenantEcommerce.Application.Users.Employees.Queries.GetByRole;
+public class GetEmployeesByRolesQueryHandler : IQueryHandler<GetEmployeesByRolesQuery, List<EmployeeResponseDTO>>
+{
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly EmployeeMapper _employeeMapper;
+
+    public GetEmployeesByRolesQueryHandler(IEmployeeRepository employeeRepository, EmployeeMapper employeeMapper)
+    {
+        _employeeRepository = employeeRepository;
+        _employeeMapper = employeeMapper;
+    }
+
+    public async Task<List<EmployeeResponseDTO>> Handle(GetEmployeesByRolesQuery request, CancellationToken cancellationToken)
+    {
+        var employees = await _employeeRepository.GetEmployeesByRole(request.roleId);
+
+        return _employeeMapper.ToEmployeeResponseDTOList(employees);
+    }
+}
