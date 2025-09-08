1480d5800ed76144ab470bd30056ae9c14574624
david <didrijo@gmail.com>
2025-09-08 22:30:06 +0100

    Feat: create interface for token service

---
+++ MultiTenantEcommerce.Application/Common/Interfaces/Services/ITokenService.cs
@@ -0,0 +1,7 @@
+﻿using MultiTenantEcommerce.Domain.Users.Entities;
+
+namespace MultiTenantEcommerce.Application.Common.Interfaces.Services;
+public interface ITokenService
+{
+    string CreateToken(UserBase user);
+}
