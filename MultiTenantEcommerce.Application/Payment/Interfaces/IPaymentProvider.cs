8d15508ee4fb3e77da0baa527525eb670eec235a
david <didrijo@gmail.com>
2025-09-08 21:58:45 +0100

    Feat: create IPaymentProvider interface with CreatePaymentAsync method

---
+++ MultiTenantEcommerce.Application/Payment/Interfaces/IPaymentProvider.cs
@@ -0,0 +1,8 @@
+﻿using MultiTenantEcommerce.Application.Payment.DTOs;
+using MultiTenantEcommerce.Domain.Sales.Orders.Entities;
+
+namespace MultiTenantEcommerce.Application.Payment.Interfaces;
+public interface IPaymentProvider
+{
+    Task<PaymentResultDTO> CreatePaymentAsync(Guid PaymentId, Order order, string tenantPaymentProviderAccountId);
+}
