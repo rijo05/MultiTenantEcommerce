ad81624787c852843cde3021311fe6206cb1eca2
david <didrijo@gmail.com>
2025-09-08 23:00:31 +0100

    Feat: create cart repository and its interface

---
+++ MultiTenantEcommerce.Domain/Sales/ShoppingCart/Interfaces/ICartRepository.cs
@@ -0,0 +1,8 @@
+﻿using MultiTenantEcommerce.Domain.Common.Interfaces;
+using MultiTenantEcommerce.Domain.Sales.ShoppingCart.Entities;
+
+namespace MultiTenantEcommerce.Domain.Sales.ShoppingCart.Interfaces;
+public interface ICartRepository : IRepository<Cart>
+{
+    Task<Cart?> GetByCustomerIdAsync(Guid customerId);
+}
---
+++ MultiTenantEcommerce.Infrastructure/Persistence/Repositories/CartRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiTenantEcommerce.Domain.Sales.ShoppingCart.Entities;
+using MultiTenantEcommerce.Domain.Sales.ShoppingCart.Interfaces;
+using MultiTenantEcommerce.Infrastructure.Persistence.Context;
+
+namespace MultiTenantEcommerce.Infrastructure.Persistence.Repositories;
+public class CartRepository : Repository<Cart>, ICartRepository
+{
+    public CartRepository(AppDbContext appDbContext, TenantContext tenantContext)
+        : base(appDbContext, tenantContext) { }
+
+    public async Task<Cart?> GetByCustomerIdAsync(Guid customerId)
+    {
+        return await _appDbContext.Carts
+            .Include(x => x.Items)
+            .ThenInclude(x => x.Product)
+            .FirstOrDefaultAsync(x => x.IsOpen && x.CustomerId == customerId);
+    }
+}
