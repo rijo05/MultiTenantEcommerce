c01e4390ed5921499778338bbf8494df21f3396f
david <didrijo@gmail.com>
2025-09-08 23:04:27 +0100

    feat: create CartItemConfiguration and CartConfiguration

---
+++ MultiTenantEcommerce.Infrastructure/Persistence/Configurations/CartConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MultiTenantEcommerce.Domain.Sales.ShoppingCart.Entities;
+using MultiTenantEcommerce.Domain.Tenants.Entities;
+using MultiTenantEcommerce.Infrastructure.Persistence.Context;
+
+namespace MultiTenantEcommerce.Infrastructure.Persistence.Configurations;
+public class CartConfiguration : IEntityTypeConfiguration<Cart>
+{
+    private readonly TenantContext _tenantContext;
+
+    public CartConfiguration(TenantContext tenantContext)
+    {
+        _tenantContext = tenantContext;
+    }
+    public void Configure(EntityTypeBuilder<Cart> builder)
+    {
+        builder.HasKey(x => new { x.TenantId, x.Id });
+
+        builder.HasOne<Tenant>()
+            .WithMany()
+            .HasForeignKey(x => x.TenantId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(x => x.Items)
+               .WithOne()
+               .HasForeignKey(x => new { x.TenantId, x.CartId })
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasQueryFilter(x => x.TenantId == _tenantContext.TenantId);
+    }
+}
---
+++ MultiTenantEcommerce.Infrastructure/Persistence/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MultiTenantEcommerce.Domain.Sales.ShoppingCart.Entities;
+using MultiTenantEcommerce.Infrastructure.Persistence.Context;
+
+namespace MultiTenantEcommerce.Infrastructure.Persistence.Configurations;
+public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+{
+    private readonly TenantContext _tenantContext;
+
+    public CartItemConfiguration(TenantContext tenantContext)
+    {
+        _tenantContext = tenantContext;
+    }
+    public void Configure(EntityTypeBuilder<CartItem> builder)
+    {
+        builder.HasKey(x => new { x.TenantId, x.CartId, x.ProductId });
+
+        builder.HasOne<Cart>()
+                .WithMany(c => c.Items)
+                .HasForeignKey(ci => new { ci.TenantId, ci.CartId });
+
+        builder.HasOne(x => x.Product)
+                .WithMany()
+                .HasForeignKey(x => new { x.TenantId, x.ProductId });
+
+        builder.OwnsOne(x => x.Quantity, qty =>
+        {
+            qty.Property(q => q.Value)
+               .HasColumnName("Quantity")
+               .IsRequired();
+        });
+
+        builder.HasQueryFilter(x => x.TenantId == _tenantContext.TenantId);
+    }
+}
