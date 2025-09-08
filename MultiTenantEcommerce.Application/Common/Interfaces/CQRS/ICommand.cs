ac6f6de33e445ede7a61a57f0bed1da7b3682a64
david <didrijo@gmail.com>
2025-09-08 21:52:35 +0100

    chore: move ICommandHandler and IQueryHandler interfaces to CQRS folder

--- MultiTenantEcommerce.Application/Common/Interfaces/CQRS/ICommand.cs
+++ MultiTenantEcommerce.Application/Common/Interfaces/CQRS/ICommand.cs
@@ -1,8 +1,9 @@
 ﻿using MediatR;
 
-namespace MultiTenantEcommerce.Application.Common.Interfaces;
+namespace MultiTenantEcommerce.Application.Common.Interfaces.CQRS;
 public interface ICommand<TResponse> : IRequest<TResponse> { }
 
 public interface ICommandHandler<TCommand, TResponse>
     : IRequestHandler<TCommand, TResponse>
-    where TCommand : ICommand<TResponse>{ }
+    where TCommand : ICommand<TResponse>
+{ }
--- MultiTenantEcommerce.Application/Common/Interfaces/CQRS/IQuery.cs
+++ MultiTenantEcommerce.Application/Common/Interfaces/CQRS/IQuery.cs
@@ -1,8 +1,9 @@
 ﻿using MediatR;
 
-namespace MultiTenantEcommerce.Application.Common.Interfaces;
+namespace MultiTenantEcommerce.Application.Common.Interfaces.CQRS;
 public interface IQuery<TResponse> : IRequest<TResponse> { }
 
 public interface IQueryHandler<TQuery, TResponse>
     : IRequestHandler<TQuery, TResponse>
-    where TQuery : IQuery<TResponse>{ }
+    where TQuery : IQuery<TResponse>
+{ }
