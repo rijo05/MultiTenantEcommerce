6f7c227172c13026c350301254c2f4624ca7521a
david <didrijo@gmail.com>
2025-09-08 21:57:03 +0100

    Feat: create PaymentProviderFactory to initialize payment providers

---
+++ MultiTenantEcommerce.Application/Payment/Interfaces/IPaymentProviderFactory.cs
@@ -0,0 +1,7 @@
+﻿using MultiTenantEcommerce.Domain.Enums;
+
+namespace MultiTenantEcommerce.Application.Payment.Interfaces;
+public interface IPaymentProviderFactory
+{
+    IPaymentProvider GetProvider(PaymentMethod method);
+}
---
+++ MultiTenantEcommerce.Infrastructure/Payments/PaymentProviderFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MultiTenantEcommerce.Application.Payment.Interfaces;
+using MultiTenantEcommerce.Domain.Enums;
+
+namespace MultiTenantEcommerce.Infrastructure.Payments;
+public class PaymentProviderFactory : IPaymentProviderFactory
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public PaymentProviderFactory(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public IPaymentProvider GetProvider(PaymentMethod method)
+    {
+        return method switch
+        {
+            PaymentMethod.Stripe => _serviceProvider.GetRequiredService<StripePaymentProvider>(),
+            _ => throw new NotSupportedException()
+        };
+    }
+}
