9586da34a2d4b1ef6a8e69054a3877113e7b90e9
david <didrijo@gmail.com>
2025-09-08 22:26:05 +0100

    Feat: create cart command validator

---
+++ MultiTenantEcommerce.Application/Sales/ShoppingCart/Validators/CreateCartCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using MultiTenantEcommerce.Application.Common.Helpers.Validators;
+using MultiTenantEcommerce.Application.Sales.ShoppingCart.Commands.Create;
+namespace MultiTenantEcommerce.Application.Sales.ShoppingCart.Validators;
+public class CreateCartCommandValidator : AbstractValidator<CreateCartCommand>
+{
+    public CreateCartCommandValidator()
+    {
+        RuleFor(x => x.CustomerId).GuidRules();
+    }
+}
