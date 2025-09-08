3f307663cacf41ac8d1bb1ddca1e8babe1ea2100
david <didrijo@gmail.com>
2025-09-08 21:30:22 +0100

    Feat: get categories to use in Product constructors

--- MultiTenantEcommerce.Application/Catalog/Products/Commands/CreateBulk/CreateProductBulkCommandHandler.cs
+++ MultiTenantEcommerce.Application/Catalog/Products/Commands/CreateBulk/CreateProductBulkCommandHandler.cs
@@ -1,50 +1,61 @@
-﻿using MultiTenantEcommerce.Application.Catalog.Commands.Product.Create;
-using MultiTenantEcommerce.Application.Catalog.Products.DTOs;
+﻿using MultiTenantEcommerce.Application.Catalog.Products.DTOs;
 using MultiTenantEcommerce.Application.Catalog.Products.Mappers;
 using MultiTenantEcommerce.Application.Common.Interfaces;
+using MultiTenantEcommerce.Application.Common.Interfaces.CQRS;
+using MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
 using MultiTenantEcommerce.Domain.Catalog.Entities;
 using MultiTenantEcommerce.Domain.Catalog.Interfaces;
-using MultiTenantEcommerce.Domain.Common.Interfaces;
-using MultiTenantEcommerce.Domain.Inventory.Entities;
-using MultiTenantEcommerce.Domain.Inventory.Interfaces;
 using MultiTenantEcommerce.Domain.ValueObjects;
-using MultiTenantEcommerce.Infrastructure.Persistence.Context;
 
 namespace MultiTenantEcommerce.Application.Catalog.Products.Commands.CreateBulk;
 public class CreateProductBulkCommandHandler : ICommandHandler<CreateProductBulkCommand, List<ProductResponseDTO>>
 {
     private readonly IProductRepository _productRepository;
-    private readonly TenantContext _tenantContext;
+    private readonly ITenantContext _tenantContext;
     private readonly IUnitOfWork _unitOfWork;
     private readonly ProductMapper _productMapper;
+    private readonly ICategoryRepository _categoryRepository;
 
     public CreateProductBulkCommandHandler(
-        IProductRepository productRepository, 
+        IProductRepository productRepository,
         ProductMapper productMapper,
-        TenantContext tenantContext,
-        IUnitOfWork unitOfWork)
+        ITenantContext tenantContext,
+        IUnitOfWork unitOfWork,
+        ICategoryRepository categoryRepository)
     {
         _productRepository = productRepository;
         _productMapper = productMapper;
         _tenantContext = tenantContext;
         _unitOfWork = unitOfWork;
+        _categoryRepository = categoryRepository;
     }
 
     public async Task<List<ProductResponseDTO>> Handle(CreateProductBulkCommand request, CancellationToken cancellationToken)
     {
+        var categories = await _categoryRepository.GetByIdsAsync(request.Products.Select(x => x.CategoryId));
+
+        if (categories.Count != request.Products.Select(x => x.CategoryId).Distinct().Count())
+            throw new Exception("Some categories couldnt be find");
+
         var products = new List<Product>();
+
+        var categoryDict = categories.ToDictionary(x => x.Id);
+
 
         foreach (var cmd in request.Products)
         {
+            if (!categoryDict.TryGetValue(cmd.CategoryId, out var category))
+                throw new Exception($"Invalid category {cmd.CategoryId}");
+
             var product = new Product(
                 _tenantContext.TenantId,
                 cmd.Name,
                 new Money(cmd.Price),
-                cmd.CategoryId,
-                cmd.Description
-            );
+                category,
+                cmd.Description,
+                cmd.IsActive);
 
-            products.Add( product );
+            products.Add(product);
         }
 
         await _productRepository.AddBulkAsync(products);
