1011cac5f62cea30c44313d2c3d9a761b1f3291e
david <didrijo@gmail.com>
2025-09-08 21:31:48 +0100

    feat: add Category object in ProductResponseDTO

--- MultiTenantEcommerce.Application/Catalog/Products/DTOs/ProductResponseDTO.cs
+++ MultiTenantEcommerce.Application/Catalog/Products/DTOs/ProductResponseDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace MultiTenantEcommerce.Application.Catalog.Products.DTOs;
+﻿using MultiTenantEcommerce.Domain.Catalog.Entities;
+
+namespace MultiTenantEcommerce.Application.Catalog.Products.DTOs;
 
 public class ProductResponseDTO
 {
@@ -7,6 +9,7 @@ public class ProductResponseDTO
     public string? Description { get; init; }
     public decimal Price { get; init; }
     public Guid CategoryId { get; init; }
+    public Category Category { get; init; }
     public bool IsActive { get; init; }
     public Dictionary<string, object> Links { get; init; }
 }
