5024ca84874115ba2d43670f4db51e71f0e47218
david <didrijo@gmail.com>
2025-09-08 20:52:31 +0100

    Feat: Add stripe account Id to tenantcontext

--- MultiTenantEcommerce.Infrastructure/Persistence/Context/TenantContext.cs
+++ MultiTenantEcommerce.Infrastructure/Persistence/Context/TenantContext.cs
@@ -1,5 +1,9 @@
-﻿namespace MultiTenantEcommerce.Infrastructure.Persistence.Context;
-public class TenantContext
+﻿using MultiTenantEcommerce.Application.Common.Interfaces;
+
+namespace MultiTenantEcommerce.Infrastructure.Persistence.Context;
+public class TenantContext : ITenantContext
 {
     public Guid TenantId { get; set; }
+
+    public string? StripeAccountId { get; set; }
 }
