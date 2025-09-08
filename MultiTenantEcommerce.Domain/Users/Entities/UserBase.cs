1cc6be3d57727c6a14020691a51ecaa5222aef0d
david <didrijo@gmail.com>
2025-09-08 22:57:35 +0100

    Refactor: add Password to UserBase

--- MultiTenantEcommerce.Domain/Users/Entities/UserBase.cs
+++ MultiTenantEcommerce.Domain/Users/Entities/UserBase.cs
@@ -1,40 +1,42 @@
-﻿using MediatR;
-using MultiTenantEcommerce.Domain.Common.Entities;
+﻿using MultiTenantEcommerce.Domain.Common.Entities;
 using MultiTenantEcommerce.Domain.Common.Guard;
-using MultiTenantEcommerce.Domain.Users.Interfaces;
 using MultiTenantEcommerce.Domain.ValueObjects;
 
 namespace MultiTenantEcommerce.Domain.Users.Entities;
 public abstract class UserBase : TenantBase
 {
-    public string Name { get; protected set; }
+    public string Name { get; private set; }
+    public Password Password { get; private set; }
     public Email Email { get; protected set; }
     public bool IsActive { get; protected set; }
 
     protected UserBase() { }
 
-    protected UserBase(Guid tenantId, string name, Email email) 
+    protected UserBase(Guid tenantId, string name, Password password, Email email)
         : base(tenantId)
     {
-        UserBaseGuard.AgainstNullOrEmptyName(name);
-        UserBaseGuard.AgainstInvalidEmail(email.Value);
-
         Name = name;
+        Password = password;
         Email = email;
         IsActive = true;
     }
 
-
-    public void UpdateName(string? newName)
+    public void UpdatePassword(string? password)
     {
-        UserBaseGuard.AgainstNullOrEmptyName(newName);
-        Name = newName;
+        Password.UpdatePassword(password);
         SetUpdatedAt();
     }
 
     public void UpdateEmail(string? newEmail)
     {
         Email.UpdateEmail(newEmail);
+        SetUpdatedAt();
+    }
+
+    public void UpdateName(string? newName)
+    {
+        UserBaseGuard.AgainstNullOrEmptyName(newName);
+        Name = newName;
         SetUpdatedAt();
     }
 
