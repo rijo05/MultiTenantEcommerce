0927297705d2cf50433f6fb991b39436c6c3833e
david <didrijo@gmail.com>
2025-09-08 22:26:54 +0100

    Feat: create stock service and its interface

---
+++ MultiTenantEcommerce.Application/Common/Interfaces/Services/IStockService.cs
@@ -0,0 +1,9 @@
+﻿using MultiTenantEcommerce.Domain.Sales.ShoppingCart.Entities;
+using MultiTenantEcommerce.Domain.ValueObjects;
+
+namespace MultiTenantEcommerce.Application.Common.Interfaces.Services;
+public interface IStockService
+{
+    public Task<bool> CheckAvailability(Guid productId, PositiveQuantity quantity);
+    Task<bool> TryReserveStockWithRetries(List<CartItem> items, int retries = 3);
+}
---
+++ MultiTenantEcommerce.Application/Inventory/Services/StockService.cs
@@ -0,0 +1,80 @@
+﻿using MultiTenantEcommerce.Application.Common.Exceptions;
+using MultiTenantEcommerce.Application.Common.Interfaces.Services;
+using MultiTenantEcommerce.Domain.Inventory.Interfaces;
+using MultiTenantEcommerce.Domain.Sales.ShoppingCart.Entities;
+using MultiTenantEcommerce.Domain.ValueObjects;
+
+namespace MultiTenantEcommerce.Application.Inventory.Services;
+public class StockService : IStockService
+{
+    private readonly IStockRepository _stockRepository;
+
+    public StockService(IStockRepository stockRepository)
+    {
+        _stockRepository = stockRepository;
+    }
+
+    public async Task<bool> CheckAvailability(Guid productId, PositiveQuantity quantity)
+    {
+        var stock = await _stockRepository.GetByIdAsync(productId)
+            ?? throw new Exception("Product doesnt exist.");
+
+        return stock.CheckAvailability(quantity.Value);
+    }
+
+    public async Task<bool> TryReserveStockWithRetries(List<CartItem> items, int retries = 3)
+    {
+        var pendingItems = new List<CartItem>(items);
+
+        for (int attempt = 0; attempt < retries; attempt++)
+        {
+            var failedItems = new List<CartItem>();
+
+            var stocks = await _stockRepository.GetBulkByIdsAsync(pendingItems.Select(x => x.Product.Id).ToList());
+            var stockDict = stocks.ToDictionary(x => x.ProductId);
+
+            foreach (var item in pendingItems)
+            {
+                if (!stockDict.TryGetValue(item.Product.Id, out var stock))
+                    throw new Exception($"Garantia extra NUNCA deve entrar aqui");
+
+                bool reserved = false;
+
+                for (int prodAttempt = 0; prodAttempt < retries; prodAttempt++)
+                {
+                    try
+                    {
+                        stock.ReserveStock(item.Quantity.Value);
+                        _stockRepository.UpdateWithRow(stock);
+                        reserved = true;
+                        break;
+                    }
+                    catch (ConcurrencyException)
+                    {
+                        if (prodAttempt < retries - 1)
+                            await Task.Delay(200);
+                    }
+                    catch (/*InsufficientStockException*/ Exception)
+                    {
+                        reserved = false;
+                        break;
+                    }
+                }
+
+                if (!reserved)
+                    failedItems.Add(item);
+            }
+
+            if (!failedItems.Any())
+                return true;
+
+            pendingItems = failedItems;
+
+            if (attempt < retries - 1)
+                await Task.Delay(500);
+        }
+
+        return false;
+    }
+
+}
