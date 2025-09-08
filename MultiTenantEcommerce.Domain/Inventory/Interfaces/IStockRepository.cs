c68ba21d183abb45b5b99fa4f1b34370d305d9df
david <didrijo@gmail.com>
2025-09-08 23:20:34 +0100

    refactor: change method name to updaterow in stock repository interface

--- MultiTenantEcommerce.Domain/Inventory/Interfaces/IStockRepository.cs
+++ MultiTenantEcommerce.Domain/Inventory/Interfaces/IStockRepository.cs
@@ -7,5 +7,5 @@ public interface IStockRepository : IRepository<Stock>
     public Task<Stock?> GetByProductIdAsync(Guid productId);
     public Task AddBulkAsync(List<Stock> items);
     public Task<List<Stock>> GetBulkByIdsAsync(List<Guid> ids);
-    public Task SaveAsync(Stock stock);
+    public void UpdateWithRow(Stock stock);
 }
