a77be2f989e841cb96f587026f6f5685dadde40b
david <didrijo@gmail.com>
2025-09-08 22:59:13 +0100

    feat: Create repository methods to get employees with associated roles

--- MultiTenantEcommerce.Domain/Users/Interfaces/IEmployeeRepository.cs
+++ MultiTenantEcommerce.Domain/Users/Interfaces/IEmployeeRepository.cs
@@ -9,6 +9,11 @@ public interface IEmployeeRepository : IRepository<Employee>
 {
     public Task<Employee?> GetByEmailAsync(Email email);
 
+    public Task<Employee?> GetByIdWithRolesAsync(Guid employeeId);
+    public Task<Employee?> GetByEmailWithRolesAsync(Email email);
+    public Task<List<Employee>> GetEmployeesByRole(Guid roleId);
+    public Task<bool> HasEmployeesWithRole(Guid roleId);
+
     public Task<List<Employee>> GetFilteredAsync(
         string? name = null,
         string? role = null,
--- MultiTenantEcommerce.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
+++ MultiTenantEcommerce.Infrastructure/Persistence/Repositories/EmployeeRepository.cs
@@ -9,7 +9,7 @@ namespace MultiTenantEcommerce.Infrastructure.Persistence.Repositories;
 public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
 {
     public EmployeeRepository(AppDbContext appDbContext, TenantContext tenantContext) : base(appDbContext, tenantContext) { }
-            
+
     public async Task<Employee?> GetByEmailAsync(Email email)
     {
         return await _appDbContext.Employees.FirstOrDefaultAsync(u => u.Email.Value == email.Value);
@@ -24,7 +24,11 @@ public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
     int pageSize = 20,
     SortOptions sort = SortOptions.TimeDesc)
     {
-        var query = _appDbContext.Employees.AsQueryable();
+        var query = _appDbContext.Employees
+            .Include(e => e.EmployeeRoles)
+            .ThenInclude(er => er.Role)
+            .ThenInclude(r => r.Permissions)
+            .AsQueryable();
 
         if (!string.IsNullOrWhiteSpace(name))
             query = query.Where(p => EF.Functions.Like(p.Name, $"%{name}%"));
@@ -36,9 +40,43 @@ public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
             query = query.Where(p => p.Email != null && EF.Functions.Like(p.Email.Value, $"%{email}%"));
 
         if (!string.IsNullOrWhiteSpace(role))
-            query = query.Where(p => p.Role != null && EF.Functions.Like(p.Role.roleName.ToString(), $"%{role}%"));
+            query = query.Where(p => p.EmployeeRoles
+                .Any(x => EF.Functions.Like(x.Role.Name, $"%{role}%")));
 
 
         return await SortAndPageAsync(query, sort, page, pageSize);
+    }
+
+    public async Task<Employee?> GetByIdWithRolesAsync(Guid employeeId)
+    {
+        return await _appDbContext.Employees
+            .Include(x => x.EmployeeRoles)
+                .ThenInclude(x => x.Role)
+                    .ThenInclude(x => x.Permissions)
+            .FirstOrDefaultAsync(x => x.Id == employeeId);
+    }
+
+    public async Task<Employee?> GetByEmailWithRolesAsync(Email email)
+    {
+        return await _appDbContext.Employees
+            .Include(x => x.EmployeeRoles)
+                .ThenInclude(x => x.Role)
+                    .ThenInclude(x => x.Permissions)
+            .FirstOrDefaultAsync(x => x.Email.Value == email.Value);
+    }
+
+    public async Task<List<Employee>> GetEmployeesByRole(Guid roleId)
+    {
+        return await _appDbContext.Employees
+            .Include(x => x.EmployeeRoles)
+                .ThenInclude(x => x.Role)
+                    .ThenInclude(x => x.Permissions)
+            .Where(x => x.EmployeeRoles.Any(x => x.RoleId == roleId))
+            .ToListAsync();
+    }
+
+    public async Task<bool> HasEmployeesWithRole(Guid roleId)
+    {
+        return await _appDbContext.Employees.AnyAsync(e => e.EmployeeRoles.Any(er => er.RoleId == roleId));
     }
 }
