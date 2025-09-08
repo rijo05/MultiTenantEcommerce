2224e2d7bd6286c5c3589421826d0886802f6efa
david <didrijo@gmail.com>
2025-09-08 21:53:40 +0100

    Feat: create AssignRoleToEmployeeCommand and its handler

---
+++ MultiTenantEcommerce.Application/Users/Employees/Commands/AssignRole/AssignRoleToEmployeeCommand.cs
@@ -0,0 +1,7 @@
+﻿using MultiTenantEcommerce.Application.Common.Interfaces.CQRS;
+using MultiTenantEcommerce.Application.Users.Employees.DTOs;
+
+namespace MultiTenantEcommerce.Application.Users.Employees.Commands.AssignRole;
+public record AssignRoleToEmployeeCommand(
+    Guid employeeId,
+    List<Guid> roles) : ICommand<EmployeeResponseDTO>;
