a713d271437e3ee6fdf12a1bb0726f8d9dfa1556
david <didrijo@gmail.com>
2025-09-23 22:15:16 +0100

    feat: add "Failed" status to order status transitions validator

--- MultiTenantEcommerce.Domain/Common/Guard/OrderStatusTransitionValidator.cs
+++ MultiTenantEcommerce.Domain/Common/Guard/OrderStatusTransitionValidator.cs
@@ -6,12 +6,13 @@ public static class OrderStatusTransitionValidator
 {
     private static readonly Dictionary<OrderStatus, List<OrderStatus>> AllowedTransitions = new()
     {
-        {OrderStatus.PendingPayment, new List<OrderStatus>() {OrderStatus.Cancelled, OrderStatus.Processing } },
+        {OrderStatus.PendingPayment, new List<OrderStatus>() {OrderStatus.Cancelled, OrderStatus.Processing, OrderStatus.Failed } },
         {OrderStatus.Processing, new List<OrderStatus>() {OrderStatus.Cancelled, OrderStatus.Shipped } },
         {OrderStatus.Shipped, new List<OrderStatus>() {OrderStatus.Delivered } },
         {OrderStatus.Delivered, new List<OrderStatus>() {OrderStatus.Invoiced } },
         {OrderStatus.Invoiced, new List<OrderStatus>() },
-        {OrderStatus.Cancelled, new List<OrderStatus>() }
+        {OrderStatus.Cancelled, new List<OrderStatus>() },
+        {OrderStatus.Failed, new List<OrderStatus>() }
     };
 
     public static void ValidateTransition(OrderStatus CurrentStatus, OrderStatus newStatus)
