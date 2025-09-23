160c398c90325a2f48e30706a3059c9aa04ec050
david <didrijo@gmail.com>
2025-09-23 21:47:22 +0100

    feat: add StockMapper

---
+++ MultiTenantEcommerce.Application/Inventory/Mappers/StockMapper.cs
@@ -0,0 +1,41 @@
+﻿using MultiTenantEcommerce.Application.Inventory.DTOs;
+using MultiTenantEcommerce.Domain.Inventory.Entities;
+
+namespace MultiTenantEcommerce.Application.Inventory.Mappers;
+public class StockMapper
+{
+    public StockResponseDTO ToStockResponseDTO(Stock stock)
+    {
+        return new StockResponseDTO()
+        {
+            AvailableStock = stock.StockAvailableAtMoment.Value,
+        };
+    }
+
+    public StockResponseAdminDTO ToStockResponseAdminDTO(Stock stock)
+    {
+        return new StockResponseAdminDTO()
+        {
+            AvailableStock = stock.StockAvailableAtMoment.Value,
+            MinimumQuantity = stock.MinimumQuantity.Value,
+            Quantity = stock.Quantity.Value,
+            Reserved = stock.Reserved.Value
+        };
+    }
+
+    public StockMovementResponseDTO ToStockMovementResponseDTO(StockMovement movement)
+    {
+        return new StockMovementResponseDTO()
+        {
+            ProductId = movement.ProductId,
+            Quantity = movement.Quantity,
+            DateTime = movement.CreatedAt,
+            Reason = movement.Reason.ToString(),
+        };
+    }
+
+    public List<StockMovementResponseDTO> ToStockMovementResponseDTOList(IEnumerable<StockMovement> movements)
+    {
+        return movements.Select(x => ToStockMovementResponseDTO(x)).ToList();
+    }
+}
