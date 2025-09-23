f0b64e1045fafbb917164ddcb97434a019246b3c
david <didrijo@gmail.com>
2025-09-23 21:35:31 +0100

    chore: reorder properties in AuthEmployeeResponseDTO

--- MultiTenantEcommerce.Application/Auth/DTOs/AuthEmployeeResponseDTO.cs
+++ MultiTenantEcommerce.Application/Auth/DTOs/AuthEmployeeResponseDTO.cs
@@ -4,7 +4,7 @@ public class AuthEmployeeResponseDTO
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
-    public string Token { get; set; }
     public List<string> Roles { get; set; }
     public List<string> Permissions { get; set; }
+    public string Token { get; set; }
 }
