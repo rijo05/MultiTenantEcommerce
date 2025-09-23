ce28d9a77d904de95cb2dd728bb45a6c8406f4cc
david <didrijo@gmail.com>
2025-09-23 22:18:04 +0100

    feat: add company Email to Tenant and TenantRegisteredEvent

--- MultiTenantEcommerce.Domain/Tenants/Entities/Tenant.cs
+++ MultiTenantEcommerce.Domain/Tenants/Entities/Tenant.cs
@@ -1,19 +1,24 @@
-﻿using MediatR;
-using MultiTenantEcommerce.Domain.Common.Entities;
+﻿using MultiTenantEcommerce.Domain.Common.Entities;
 using MultiTenantEcommerce.Domain.Common.Guard;
+using MultiTenantEcommerce.Domain.Tenants.Events;
+using MultiTenantEcommerce.Domain.ValueObjects;
 
 namespace MultiTenantEcommerce.Domain.Tenants.Entities;
 public class Tenant : BaseEntity
 {
     public string Name { get; private set; }
+    public Email Email { get; private set; }
 
     private Tenant() { }
-    public Tenant(string companyName)
+    public Tenant(string companyName, Email companyEmail)
     {
         GuardCommon.AgainstNullOrEmpty(companyName, nameof(companyName));
-        GuardCommon.AgainstMaxLength(companyName, 50 ,nameof(companyName)); 
+        GuardCommon.AgainstMaxLength(companyName, 50, nameof(companyName));
 
         Name = companyName;
+        Email = companyEmail;
+
+        AddDomainEvent(new TenantRegisteredEvent(this.Id, Email.Value));
     }
 
     public void UpdateTenant(string companyName)
