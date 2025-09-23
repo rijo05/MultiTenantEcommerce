bc9a0222652986a5a2b6a6914017852ef62a362b
david <didrijo@gmail.com>
2025-09-23 22:01:14 +0100

    feat: add checkoutDTO

---
+++ MultiTenantEcommerce.Application/Sales/ShoppingCart/DTOs/CheckoutDTO.cs
@@ -0,0 +1,7 @@
+﻿using MultiTenantEcommerce.Application.Common.DTOs.Address;
+using MultiTenantEcommerce.Domain.Enums;
+
+namespace MultiTenantEcommerce.Application.Sales.ShoppingCart.DTOs;
+public record CheckoutDTO(
+    CreateAddressDTO AddressDTO,
+    PaymentMethod PaymentMethod);
