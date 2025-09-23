0be41ccae5515ba03a531929db4ec055805082ea
david <didrijo@gmail.com>
2025-09-23 21:33:58 +0100

    feat: add CompanyEmail to CreateTenantCommand

--- MultiTenantEcommerce.Application/Auth/Commands/CreateTenant/CreateTenantCommand.cs
+++ MultiTenantEcommerce.Application/Auth/Commands/CreateTenant/CreateTenantCommand.cs
@@ -4,6 +4,7 @@ using MultiTenantEcommerce.Application.Common.Interfaces.CQRS;
 namespace MultiTenantEcommerce.Application.Auth.Commands.CreateTenant;
 public record CreateTenantCommand(
     string CompanyName,
+    string CompanyEmail,
     string OwnerName,
     string OwnerEmail,
     string Password) : ICommand<AuthTenantResponse>;
