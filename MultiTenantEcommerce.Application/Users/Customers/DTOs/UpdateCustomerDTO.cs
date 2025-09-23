9aecde6e7b8080b5e0b4b3ce344a97116e8bed0b
david <didrijo@gmail.com>
2025-09-23 22:02:08 +0100

    refactor: change UpdateCustomerDTO from class to record

--- MultiTenantEcommerce.Application/Users/Customers/DTOs/UpdateCustomerDTO.cs
+++ MultiTenantEcommerce.Application/Users/Customers/DTOs/UpdateCustomerDTO.cs
@@ -1,12 +1,10 @@
 ﻿using MultiTenantEcommerce.Application.Common.DTOs.Address;
 
 namespace MultiTenantEcommerce.Application.Users.Customers.DTOs;
-public class UpdateCustomerDTO
-{
-    public string? Name { get; set; }
-    public string? Email { get; set; }
-    public string? Password { get; set; }
-    public string? PhoneNumber { get; set; }
-    public string? CountryCode { get; set; }
-    public CreateAddressDTO? Address { get; set; }
-}
+public record UpdateCustomerDTO(
+    string? Name,
+    string? Email,
+    string? Password,
+    string? PhoneNumber,
+    string? CountryCode,
+    CreateAddressDTO? Address);
