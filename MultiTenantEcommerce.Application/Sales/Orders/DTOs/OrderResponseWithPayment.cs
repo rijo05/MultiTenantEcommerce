878f5cca8c5e9c8d0772931a703b0d714186ce4f
david <didrijo@gmail.com>
2025-09-23 21:53:51 +0100

    refactor: rename payment to orderpayment in OrderResponse DTO

--- MultiTenantEcommerce.Application/Sales/Orders/DTOs/OrderResponseWithPayment.cs
+++ MultiTenantEcommerce.Application/Sales/Orders/DTOs/OrderResponseWithPayment.cs
@@ -1,8 +1,8 @@
-﻿using MultiTenantEcommerce.Application.Payment.DTOs;
+﻿using MultiTenantEcommerce.Application.Payment.OrderPayment.DTOs;
 
 namespace MultiTenantEcommerce.Application.Sales.Orders.DTOs;
 public class OrderResponseWithPayment
 {
     public OrderResponseDTO Order { get; set; }
-    public PaymentResponseDTO Payment { get; set; }
+    public OrderPaymentResponseDTO Payment { get; set; }
 }
