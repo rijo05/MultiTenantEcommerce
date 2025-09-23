d563dfe1fc504bcc18f386894d5284045576ed80
david <didrijo@gmail.com>
2025-09-23 21:16:51 +0100

    chore: organize controllers into admin and customer categories

--- MultiTenantEcommerce.API/Controllers/AuthController.cs
+++ MultiTenantEcommerce.API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MultiTenantEcommerce.API.Authorization;
 using MultiTenantEcommerce.Application.Auth.Commands.CreateCustomer;
 using MultiTenantEcommerce.Application.Auth.Commands.CreateEmployee;
 using MultiTenantEcommerce.Application.Auth.Commands.CreateTenant;
@@ -21,7 +22,7 @@ public class AuthController : ControllerBase
         _mediator = mediator;
     }
 
-    [HttpPost("register/customer")]
+    [HttpPost("customer/register")]
     public async Task<ActionResult<AuthCustomerResponseDTO>> RegisterCustomer([FromBody] CreateCustomerCommand customerCommand)
     {
         var result = await _mediator.Send(customerCommand);
@@ -29,23 +30,7 @@ public class AuthController : ControllerBase
         return Ok(result);
     }
 
-    [Authorize(Policy = "AdminOnly")]
-    [HttpPost("register/employee")]
-    public async Task<ActionResult<AuthEmployeeResponseDTO>> RegisterEmployee([FromBody] CreateEmployeeCommand employeeCommand)
-    {
-        var result = await _mediator.Send(employeeCommand);
-
-        return Ok(result);
-    }
-    [HttpPost("register/tenant")]
-    public async Task<ActionResult<AuthTenantResponse>> RegisterTenant([FromBody] CreateTenantCommand tenantCommand)
-    {
-        var result = await _mediator.Send(tenantCommand);
-
-        return Ok(result);
-    }
-
-    [HttpPost("login/customer")]
+    [HttpPost("customers/login")]
     public async Task<ActionResult<AuthCustomerResponseDTO>> LoginCustomer([FromBody] LoginCustomerQuery loginCustomer)
     {
         var result = await _mediator.Send(loginCustomer);
@@ -53,11 +38,33 @@ public class AuthController : ControllerBase
         return Ok(result);
     }
 
-    [HttpPost("login/employee")]
+    [HasPermission("create.employee")]
+    [Authorize(Policy = "Permission")] //############
+    [HttpPost("employees/register")]
+    public async Task<ActionResult<AuthEmployeeResponseDTO>> RegisterEmployee([FromBody] CreateEmployeeCommand employeeCommand)
+    {
+        var result = await _mediator.Send(employeeCommand);
+
+        return Ok(result);
+    }
+
+    [HttpPost("employees/login")]
     public async Task<ActionResult<AuthEmployeeResponseDTO>> LoginEmployee([FromBody] LoginEmployeeQuery loginEmployee)
     {
         var result = await _mediator.Send(loginEmployee);
 
         return Ok(result);
     }
+
+    [HttpPost("tenants/register")]
+    public async Task<ActionResult<AuthTenantResponse>> RegisterTenant([FromBody] CreateTenantCommand tenantCommand)
+    {
+        var result = await _mediator.Send(tenantCommand);
+
+        return Ok(result);
+    }
+
+    [HttpGet("test")]
+    [Authorize]
+    public IActionResult Test() => Ok("ok");
 }
