985afd9a0b1885c34f60a8b5150d05bf5ec9d8be
david <didrijo@gmail.com>
2025-09-23 22:04:55 +0100

    feat: add UpdateEmployeeAdminDTO

---
+++ MultiTenantEcommerce.Application/Users/Employees/DTOs/UpdateEmployeeAdminDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiTenantEcommerce.Application.Users.Employees.DTOs;
+
+public record UpdateEmployeeAdminDTO(
+    string? Name,
+    string? Email,
+    string? Password,
+    string? Role,
+    bool? IsActive);
