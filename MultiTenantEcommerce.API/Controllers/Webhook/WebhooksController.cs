66386a076c0de01aaaa0961a86d3867ec770b06c
david <didrijo@gmail.com>
2025-09-23 21:17:25 +0100

    chore: organize controller into webhook

---
+++ MultiTenantEcommerce.API/Controllers/Webhook/WebhooksController.cs
@@ -0,0 +1,67 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MultiTenantEcommerce.Application.Payment.OrderPayment.Commands.Completed;
+using MultiTenantEcommerce.Application.Payment.OrderPayment.Commands.Failed;
+using Stripe;
+
+namespace MultiTenantEcommerce.API.Controllers.Webhook;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WebhooksController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public WebhooksController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("stripe")]
+    public async Task<IActionResult> StripeWebhook()
+    {
+        const string endpointSecret = "whsec_...";
+
+        var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+        Event stripeEvent = EventUtility.ParseEvent(json);
+        var signatureHeader = Request.Headers["Stripe-Signature"];
+
+        stripeEvent = EventUtility.ConstructEvent(json, signatureHeader, endpointSecret);
+
+
+        if (stripeEvent.Type == EventTypes.PaymentIntentSucceeded)
+        {
+            var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+
+            var paymentId = Guid.Parse(paymentIntent.Metadata["PaymentId"]);
+
+            var payment = new MarkOrderPaymentAsCompletedCommand(paymentId, paymentIntent.Id);
+
+            await _mediator.Send(payment);
+        }
+        else if (stripeEvent.Type == EventTypes.PaymentIntentPaymentFailed)
+        {
+            var paymentIntent = stripeEvent.Data.Object as PaymentIntent;
+
+            var paymentId = Guid.Parse(paymentIntent.Metadata["PaymentId"]);
+
+            var payment = new MarkPaymentAsFailedCommand(paymentId, paymentIntent.LastPaymentError.Message);
+
+            await _mediator.Send(payment);
+        }
+        else
+        {
+            //loggar talvez ou simplesmente ignorar
+        }
+
+        return NoContent();
+    }
+
+    [HttpPost("paypal")]
+    public async Task<IActionResult> PaypalWebhook()
+    {
+
+        throw new NotImplementedException();
+    }
+}
