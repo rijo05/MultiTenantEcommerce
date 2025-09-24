7263b144ea17e81ceb7b373a04e881bd73c3ce09
david <didrijo@gmail.com>
2025-09-24 01:26:01 +0100

    feat: create handlers for OrderEvents to send emails

---
+++ MultiTenantEcommerce.Application/Sales/Orders/EventHandlers/SendEmailOnOrderDeliveredEventHandler.cs
@@ -0,0 +1,60 @@
+﻿using MultiTenantEcommerce.Application.Common.DTOs;
+using MultiTenantEcommerce.Application.Common.Helpers.EmailKeys;
+using MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
+using MultiTenantEcommerce.Domain.Enums;
+using MultiTenantEcommerce.Domain.Sales.Orders.Events;
+using MultiTenantEcommerce.Domain.Sales.Orders.Interfaces;
+using MultiTenantEcommerce.Domain.Tenants.Interfaces;
+using MultiTenantEcommerce.Domain.Users.Interfaces;
+
+namespace MultiTenantEcommerce.Application.Sales.Orders.EventHandlers;
+public class SendEmailOnOrderDeliveredEventHandler : IEventHandler<OrderDeliveredEvent>
+{
+    private readonly IEmailQueueRepository _emailQueueRepository;
+    private readonly IOrderRepository _orderRepository;
+    private readonly ICustomerRepository _customerRepository;
+    private readonly ITenantRepository _tenantRepository;
+
+    public SendEmailOnOrderDeliveredEventHandler(
+        IEmailQueueRepository emailQueueRepository,
+        IOrderRepository orderRepository,
+        ICustomerRepository customerRepository,
+        ITenantRepository tenantRepository)
+    {
+        _emailQueueRepository = emailQueueRepository;
+        _orderRepository = orderRepository;
+        _customerRepository = customerRepository;
+        _tenantRepository = tenantRepository;
+    }
+
+    public async Task HandleAsync(OrderDeliveredEvent domainEvent)
+    {
+        var order = await _orderRepository.GetByIdAsync(domainEvent.OrderId)
+            ?? throw new Exception("Order not found");
+
+        var customer = await _customerRepository.GetByIdAsync(order.CustomerId)
+            ?? throw new Exception("Customer not found");
+
+        var tenant = await _tenantRepository.GetByIdAsync(domainEvent.TenantId)
+            ?? throw new Exception("Tenant not found");
+
+        var metadata = new Dictionary<string, string>
+        {
+            [EmailMetadataKeys.CustomerName] = customer.Name,
+            [EmailMetadataKeys.OrderId] = order.Id.ToString(),
+            [EmailMetadataKeys.ItemsHtml] = string.Join("", order.Items.Select(item => $"<tr><td>{item.ProductName}</td><td>{item.Quantity}</td><td>{item.UnitPrice:C}</td></tr>")),
+            [EmailMetadataKeys.TenantName] = tenant.Name
+        };
+
+        var email = new EmailJobDataDTO(
+            Guid.Empty,
+            domainEvent.TenantId,
+            customer.Email.Value,
+            EmailTemplateNames.OrderDeliverd,
+            metadata,
+            tenant.Email.Value
+        );
+
+        await _emailQueueRepository.EnqueueEmailAsync(email);
+    }
+}
