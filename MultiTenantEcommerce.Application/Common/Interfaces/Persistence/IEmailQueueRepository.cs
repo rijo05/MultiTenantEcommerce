c84d912a7609271f71f5dd4d0bfac9bea1e9cd37
david <didrijo@gmail.com>
2025-09-25 00:42:10 +0100

    feat: implement separate RabbitMQ queues for critical and non-critical domain events

--- MultiTenantEcommerce.Application/Common/Interfaces/Persistence/IEmailQueueRepository.cs
+++ MultiTenantEcommerce.Application/Common/Interfaces/Persistence/IEmailQueueRepository.cs
@@ -1,10 +1,11 @@
 ﻿using MultiTenantEcommerce.Application.Common.DTOs;
+using MultiTenantEcommerce.Domain.Enums;
 
 namespace MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
 public interface IEmailQueueRepository
 {
     Task EnqueueEmailAsync(EmailJobDataDTO emailJob);
-    Task<List<EmailJobDataDTO>> GetBatchUnprocessedEmailsAsync(int batchSize);
+    Task<List<EmailJobDataDTO>> GetBatchUnprocessedEmailsAsync(EventPriority priority, int batchSize);
     Task MarkEmailAsSentAsync(Guid emailId);
     Task MarkEmailAsFailedAsync(Guid emailId, string errorMessage);
 }
