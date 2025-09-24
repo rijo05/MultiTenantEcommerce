b3cddf53c1a3bfa1aef7fe2b9372f65b3d74a69e
david <didrijo@gmail.com>
2025-09-24 01:30:30 +0100

    feat: create handler for StockEvents to send email

---
+++ MultiTenantEcommerce.Application/Inventory/EventHandlers/SendEmailOnLowStockEventHandler.cs
@@ -0,0 +1,52 @@
+﻿using MultiTenantEcommerce.Application.Common.DTOs;
+using MultiTenantEcommerce.Application.Common.Helpers.EmailKeys;
+using MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
+using MultiTenantEcommerce.Domain.Catalog.Interfaces;
+using MultiTenantEcommerce.Domain.Enums;
+using MultiTenantEcommerce.Domain.Inventory.Events;
+using MultiTenantEcommerce.Domain.Users.Interfaces;
+
+namespace MultiTenantEcommerce.Application.Inventory.EventHandlers;
+public class SendEmailOnLowStockEventHandler : IEventHandler<LowStockEvent>
+{
+    private readonly IEmailQueueRepository _emailQueueRepository;
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly IProductRepository _productRepository;
+
+    public SendEmailOnLowStockEventHandler(IEmailQueueRepository emailQueueRepository,
+        IEmployeeRepository employeeRepository,
+        IProductRepository productRepository)
+    {
+        _emailQueueRepository = emailQueueRepository;
+        _employeeRepository = employeeRepository;
+        _productRepository = productRepository;
+    }
+
+    public async Task HandleAsync(LowStockEvent domainEvent)
+    {
+        var owner = await _employeeRepository.GetOwnerOfTenant(domainEvent.TenantId)
+            ?? throw new Exception("Something went wrong");
+
+        var product = await _productRepository.GetByIdAsync(domainEvent.ProductId)
+            ?? throw new Exception("Something went wrong");
+
+        var metadata = new Dictionary<string, string>
+        {
+            [EmailMetadataKeys.ProductId] = domainEvent.ProductId.ToString(),
+            [EmailMetadataKeys.ProductName] = product.Name,
+            [EmailMetadataKeys.CurrentQuantity] = domainEvent.CurrentQuantity.ToString(),
+            [EmailMetadataKeys.MinimumQuantity] = domainEvent.MinimumQuantity.ToString(),
+        };
+
+        var email = new EmailJobDataDTO(
+            Guid.Empty,
+            domainEvent.TenantId,
+            owner.Email.Value,
+            EmailTemplateNames.LowStockEvent,
+            metadata,
+            ""
+        );
+
+        await _emailQueueRepository.EnqueueEmailAsync(email);
+    }
+}
---
+++ MultiTenantEcommerce.Application/Inventory/EventHandlers/SendEmailOnOutOfStockEventHandler.cs
@@ -0,0 +1,50 @@
+﻿using MultiTenantEcommerce.Application.Common.DTOs;
+using MultiTenantEcommerce.Application.Common.Helpers.EmailKeys;
+using MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
+using MultiTenantEcommerce.Domain.Catalog.Interfaces;
+using MultiTenantEcommerce.Domain.Enums;
+using MultiTenantEcommerce.Domain.Inventory.Events;
+using MultiTenantEcommerce.Domain.Users.Interfaces;
+
+namespace MultiTenantEcommerce.Application.Inventory.EventHandlers;
+public class SendEmailOnOutOfStockEventHandler : IEventHandler<OutOfStockEvent>
+{
+    private readonly IEmailQueueRepository _emailQueueRepository;
+    private readonly IEmployeeRepository _employeeRepository;
+    private readonly IProductRepository _productRepository;
+
+    public SendEmailOnOutOfStockEventHandler(IEmailQueueRepository emailQueueRepository,
+        IEmployeeRepository employeeRepository,
+        IProductRepository productRepository)
+    {
+        _emailQueueRepository = emailQueueRepository;
+        _employeeRepository = employeeRepository;
+        _productRepository = productRepository;
+    }
+
+    public async Task HandleAsync(OutOfStockEvent domainEvent)
+    {
+        var owner = await _employeeRepository.GetOwnerOfTenant(domainEvent.TenantId)
+            ?? throw new Exception("Something went wrong");
+
+        var product = await _productRepository.GetByIdAsync(domainEvent.ProductId)
+            ?? throw new Exception("Something went wrong");
+
+        var metadata = new Dictionary<string, string>
+        {
+            [EmailMetadataKeys.ProductId] = domainEvent.ProductId.ToString(),
+            [EmailMetadataKeys.ProductName] = product.Name,
+        };
+
+        var email = new EmailJobDataDTO(
+            Guid.Empty,
+            domainEvent.TenantId,
+            owner.Email.Value,
+            EmailTemplateNames.OutOfStockEvent,
+            metadata,
+            ""
+        );
+
+        await _emailQueueRepository.EnqueueEmailAsync(email);
+    }
+}
