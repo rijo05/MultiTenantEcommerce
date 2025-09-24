9aa5f5fc3eb75fdc87678360a245040d029448ae
david <didrijo@gmail.com>
2025-09-24 01:03:56 +0100

    feat: create domain events for customers

---
+++ MultiTenantEcommerce.Domain/Users/Events/CustomerRegisteredEvent.cs
@@ -0,0 +1,11 @@
+﻿using MultiTenantEcommerce.Domain.Common.Events;
+
+namespace MultiTenantEcommerce.Domain.Users.Events;
+public record CustomerRegisteredEvent(
+    Guid TenantId,
+    Guid CustomerId) : IDomainEvent, IEmailEvent
+{
+    public string TemplateName => "CustomerRegistered";
+    public Guid EventId { get; } = Guid.NewGuid();
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+}
