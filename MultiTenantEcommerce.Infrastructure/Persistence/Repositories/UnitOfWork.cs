153e39481ecfbf1a784fcffa3c9b873ff306c5ad
david <didrijo@gmail.com>
2025-09-24 01:09:27 +0100

    refactor: change CommitAsync to use OutboxEvents table instead of MediatR

--- MultiTenantEcommerce.Infrastructure/Persistence/Repositories/UnitOfWork.cs
+++ MultiTenantEcommerce.Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using MultiTenantEcommerce.Application.Common.Exceptions;
 using MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
+using MultiTenantEcommerce.Infrastructure.Outbox;
 using MultiTenantEcommerce.Infrastructure.Persistence.Context;
 using MultiTenantEcommerce.Infrastructure.Persistence.Transaction;
 using System.Data.Entity.Infrastructure;
@@ -10,12 +11,10 @@ namespace MultiTenantEcommerce.Infrastructure.Persistence.Repositories;
 public class UnitOfWork : IUnitOfWork
 {
     private readonly AppDbContext _context;
-    private readonly IMediator _mediator;
 
-    public UnitOfWork(AppDbContext context, IMediator mediator)
+    public UnitOfWork(AppDbContext context)
     {
         _context = context;
-        _mediator = mediator;
     }
 
     public async Task<ITransaction> BeginTransactionAsync()
@@ -24,24 +23,33 @@ public class UnitOfWork : IUnitOfWork
         return new EfCoreTransaction(transaction);
     }
 
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
     public async Task<int> CommitAsync()
     {
         var result = 0;
 
         try
         {
+            var events = _context.GetAllDomainEvents();
+            _context.ClearAllDomainEvents();
+
+            foreach (var domainEvent in events)
+            {
+                var outboxMessage = new OutboxEvent(domainEvent);
+
+                await _context.OutboxEvents.AddAsync(outboxMessage);
+            }
+
             result = await _context.SaveChangesAsync();
         }
         catch (DbUpdateConcurrencyException ex)
         {
             throw new ConcurrencyException("Concurrency conflict while saving changes", ex);
         }
-
-        var events = _context.GetAllDomainEvents();
-        _context.ClearAllDomainEvents();
-
-        foreach (var domainEvent in events)
-            await _mediator.Publish(domainEvent);
 
         return result;
     }
