05f7210415fbc466855abe7a9594822f7a97da71
david <didrijo@gmail.com>
2025-09-24 01:20:13 +0100

    feat: implement TemplateRenderer for template rendering using metadata dictionary

---
+++ MultiTenantEcommerce.Infrastructure/EmailService/TemplateRenderer.cs
@@ -0,0 +1,11 @@
+﻿using Scriban;
+
+namespace MultiTenantEcommerce.Infrastructure.EmailService;
+public class TemplateRenderer
+{
+    public string Render(string templateText, Dictionary<string, string> metadata)
+    {
+        var template = Template.Parse(templateText);
+        return template.Render(metadata, memberRenamer: member => member.Name);
+    }
+}
