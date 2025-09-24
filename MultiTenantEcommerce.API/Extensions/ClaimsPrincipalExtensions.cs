f9a71cf9e175a5459ca9cc16e94bc2c681b055b8
david <didrijo@gmail.com>
2025-09-24 01:32:30 +0100

    feat: implement extension methods for extracting UserId and TenantId from ClaimsPrincipal

---
+++ MultiTenantEcommerce.API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace MultiTenantEcommerce.API.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static Guid GetUserId(this ClaimsPrincipal claims) =>
+        Guid.Parse(claims.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+
+    public static Guid GetTenantId(this ClaimsPrincipal claims) =>
+        Guid.Parse(claims.FindFirst("tenantId")!.Value);
+}
