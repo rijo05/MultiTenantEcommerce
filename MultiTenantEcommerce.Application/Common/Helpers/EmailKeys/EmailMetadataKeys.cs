c0fe501e4d8800c7a214e0cca3b7a69dd6008963
david <didrijo@gmail.com>
2025-09-24 01:31:37 +0100

    feat: create EmailMetadataKeys to define constants for email template placeholders

---
+++ MultiTenantEcommerce.Application/Common/Helpers/EmailKeys/EmailMetadataKeys.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiTenantEcommerce.Application.Common.Helpers.EmailKeys;
+public static class EmailMetadataKeys
+{
+    public const string CustomerName = "CustomerName";
+    public const string TenantName = "TenantName";
+    public const string ItemsHtml = "ItemsHtml";
+    public const string OrderId = "OrderId";
+    public const string TrackingNumber = "TrackingNumber";
+    public const string ChangePasswordLink = "ChangePasswordLink";
+    public const string ProductId = "ProductId";
+    public const string ProductName = "ProductName";
+    public const string CurrentQuantity = "CurrentQuantity";
+    public const string MinimumQuantity = "MinimumQuantity";
+}
