afce74b16599dfc484c529fc132641efe2c51d54
david <didrijo@gmail.com>
2025-09-24 01:02:13 +0100

    feat: create domain events for tenant

---
+++ MultiTenantEcommerce.Domain/Tenants/Events/TenantRegisteredEvent.cs
@@ -0,0 +1,11 @@
+﻿using MultiTenantEcommerce.Domain.Common.Events;
+
+namespace MultiTenantEcommerce.Domain.Tenants.Events;
+public record TenantRegisteredEvent(
+    Guid TenantId,
+    string Email) : IDomainEvent, IEmailEvent
+{
+    public string TemplateName => "TenantRegistered";
+    public Guid EventId { get; } = Guid.NewGuid();
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+}
