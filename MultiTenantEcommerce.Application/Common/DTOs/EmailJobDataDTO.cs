d5c8958e842338aab2e3ee5551a6469966225253
david <didrijo@gmail.com>
2025-09-24 01:31:59 +0100

    feat: create EmailJobDataDTO

---
+++ MultiTenantEcommerce.Application/Common/DTOs/EmailJobDataDTO.cs
@@ -0,0 +1,11 @@
+﻿using MultiTenantEcommerce.Domain.Enums;
+
+namespace MultiTenantEcommerce.Application.Common.DTOs;
+public record EmailJobDataDTO(
+    Guid Id,
+    Guid TenantId,
+    string ToEmail,
+    EmailTemplateNames TemplateName,
+    Dictionary<string, string> Metadata,
+    string? ReplyToEmail = null
+);
