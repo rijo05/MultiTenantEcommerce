f97b8c97b149dc497e06ba184d58d9d820238679
david <didrijo@gmail.com>
2025-09-24 01:06:19 +0100

    feat: create SetTenantId

--- MultiTenantEcommerce.Infrastructure/Persistence/Context/TenantContext.cs
+++ MultiTenantEcommerce.Infrastructure/Persistence/Context/TenantContext.cs
@@ -1,9 +1,12 @@
-﻿using MultiTenantEcommerce.Application.Common.Interfaces;
+﻿using MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
 
 namespace MultiTenantEcommerce.Infrastructure.Persistence.Context;
 public class TenantContext : ITenantContext
 {
     public Guid TenantId { get; set; }
 
-    public string? StripeAccountId { get; set; }
+    public void SetTenantId(Guid tenantId)
+    {
+        TenantId = tenantId;
+    }
 }
