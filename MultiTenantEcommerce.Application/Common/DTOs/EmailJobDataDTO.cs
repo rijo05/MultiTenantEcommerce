00aa5e17b024a75fe53c47e2606e884b5bce1719
david <didrijo@gmail.com>
2025-09-25 00:36:01 +0100

    feat: add FromName and Priority to EmailJobDataDTO

--- MultiTenantEcommerce.Application/Common/DTOs/EmailJobDataDTO.cs
+++ MultiTenantEcommerce.Application/Common/DTOs/EmailJobDataDTO.cs
@@ -4,8 +4,10 @@ namespace MultiTenantEcommerce.Application.Common.DTOs;
 public record EmailJobDataDTO(
     Guid Id,
     Guid TenantId,
+    string FromName,
     string ToEmail,
     EmailTemplateNames TemplateName,
+    EventPriority Priority,
     Dictionary<string, string> Metadata,
     string? ReplyToEmail = null
 );
