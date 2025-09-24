036e9ce58146cff501a5ae1525226f01bb000859
david <didrijo@gmail.com>
2025-09-24 01:04:06 +0100

    feat: create domain events for employees

---
+++ MultiTenantEcommerce.Domain/Users/Events/EmployeeRegisteredEvent.cs
@@ -0,0 +1,11 @@
+﻿using MultiTenantEcommerce.Domain.Common.Events;
+
+namespace MultiTenantEcommerce.Domain.Users.Events;
+public record EmployeeRegisteredEvent(
+    Guid TenantId,
+    Guid EmployeeId) : IDomainEvent, IEmailEvent
+{
+    public string TemplateName => "EmployeeRegistered";
+    public Guid EventId { get; } = Guid.NewGuid();
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+}
