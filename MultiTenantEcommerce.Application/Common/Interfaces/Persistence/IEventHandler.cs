3d100a54ecd13e950cf83a2bd4c68bb20f204d10
david <didrijo@gmail.com>
2025-09-24 01:29:52 +0100

    feat: create IEventHandler interface for processing domain events

---
+++ MultiTenantEcommerce.Application/Common/Interfaces/Persistence/IEventHandler.cs
@@ -0,0 +1,7 @@
+﻿using MultiTenantEcommerce.Domain.Common.Events;
+
+namespace MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
+public interface IEventHandler<TDomainEvent> where TDomainEvent : IDomainEvent
+{
+    public Task HandleAsync(TDomainEvent domainEvent);
+}
