3d2a04d4b98c43d893d3e76ad7f5e3e8f0a895c7
david <didrijo@gmail.com>
2025-09-24 01:21:37 +0100

    feat: implement EmailSender to send emails using MailKit

---
+++ MultiTenantEcommerce.Application/Common/Interfaces/Persistence/IEmailSender.cs
@@ -0,0 +1,5 @@
+﻿namespace MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
+public interface IEmailSender
+{
+    public Task SendAsync(string toEmail, string subject, string htmlBody, string textBody, string? replyToEmail = null);
+}
---
+++ MultiTenantEcommerce.Infrastructure/EmailService/EmailSender.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
+using System.Runtime.CompilerServices;
+
+namespace MultiTenantEcommerce.Infrastructure.EmailService;
+public class EmailSender : IEmailSender
+{
+    private readonly string _fromEmail;
+    private readonly string _fromName;
+    private readonly string _appPassword;
+
+    public EmailSender(IConfiguration config)
+    {
+        _fromEmail = config["Email:Gmail:FromEmail"];
+        _fromName = config["Email:Gmail:FromName"];
+        _appPassword = config["Email:Gmail:AppPassword"];
+    }
+
+    public async Task SendAsync(string toEmail, string subject, string htmlBody, string textBody, string? replyToEmail = null)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_fromName, _fromEmail));
+        message.To.Add(new MailboxAddress("", toEmail));
+        message.Subject = subject;
+
+        if (!string.IsNullOrEmpty(replyToEmail))
+            message.ReplyTo.Add(MailboxAddress.Parse(replyToEmail));
+
+        var builder = new BodyBuilder
+        {
+            HtmlBody = htmlBody,
+            TextBody = textBody
+        };
+
+        message.Body = builder.ToMessageBody();
+
+        using var client = new SmtpClient();
+        await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(_fromEmail, _appPassword);
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+    }
+}
