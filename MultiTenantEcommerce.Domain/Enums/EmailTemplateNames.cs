08100f6bc8092b82d247d4718c4ec9683648b06f
david <didrijo@gmail.com>
2025-09-24 01:04:33 +0100

    feat: create EmailTemplateNames enum

---
+++ MultiTenantEcommerce.Domain/Enums/EmailTemplateNames.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiTenantEcommerce.Domain.Enums;
+public enum EmailTemplateNames
+{
+    LowStockEvent,
+    OutOfStockEvent,
+    OrderDeliverd,
+    OrderPaid,
+    OrderPaymentFailed,
+    OrderShipped,
+    CustomerRegistered,
+    EmployeeRegistered
+}
