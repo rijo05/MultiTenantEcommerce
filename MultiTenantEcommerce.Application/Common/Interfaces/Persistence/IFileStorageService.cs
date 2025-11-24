a1637464acbb65fef9c7e033003b46a1b52ae3e0
david <didrijo@gmail.com>
2025-11-24 16:40:34 +0000

    feat: implement file storage service using cloudflare r2

---
+++ MultiTenantEcommerce.Application/Common/Interfaces/Persistence/IFileStorageService.cs
@@ -0,0 +1,12 @@
+﻿using MultiTenantEcommerce.Application.Common.DTOs;
+using MultiTenantEcommerce.Domain.Catalog.Entities;
+
+namespace MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
+public interface IFileStorageService
+{
+    public List<PresignedUpload> GenerateUploadUrls(Guid tenantId, Guid productId, List<ProductImages> images);
+    public string GetImageUrl(string key);
+    public Task DeleteImageUrl(string key);
+    public List<string> GetImageUrl(List<string> key);
+    public Task UploadAsync(string key, byte[] content, string contentType);
+}
---
+++ MultiTenantEcommerce.Infrastructure/Storage/FileStorageService.cs
@@ -0,0 +1,134 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Configuration;
+using MultiTenantEcommerce.Application.Common.DTOs;
+using MultiTenantEcommerce.Application.Common.Interfaces.Persistence;
+using MultiTenantEcommerce.Domain.Catalog.Entities;
+
+namespace MultiTenantEcommerce.Infrastructure.Storage;
+public class FileStorageService : IFileStorageService
+{
+    private BasicAWSCredentials _credentials;
+    private string _serviceURL = "";
+    private string _bucketName = "";
+
+    public FileStorageService(IConfiguration configuration)
+    {
+        var acessKey = configuration["Cloudflare:AccessKeyId"];
+        var secretKey = configuration["Cloudflare:SecretAccessKey"];
+        _serviceURL = configuration["Cloudflare:ServiceUrl"];
+        _bucketName = configuration["Cloudflare:BucketName"];
+
+        _credentials = new BasicAWSCredentials(acessKey, secretKey);
+    }
+
+    public async Task DeleteImageUrl(string key)
+    {
+        var client = new AmazonS3Client();
+
+        var deleteRequest = new DeleteObjectRequest
+        {
+            BucketName = _bucketName,
+            Key = Uri.UnescapeDataString(key)
+        };
+
+        var response = await client.DeleteObjectAsync(deleteRequest);
+    }
+
+
+    public List<PresignedUpload> GenerateUploadUrls(Guid tenantId, Guid productId, List<ProductImages> images)
+    {
+        var client = ConnectClient();
+
+        var uploads = new List<PresignedUpload>();
+
+        foreach (var img in images)
+        {
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = _bucketName,
+                Key = img.Key,
+                Verb = HttpVerb.PUT,
+                Expires = DateTime.UtcNow.AddMinutes(10),
+                ContentType = img.ContentType
+            };
+
+            var url = client.GetPreSignedURL(request);
+
+            uploads.Add(new PresignedUpload(img.Key, url, img.ContentType));
+        }
+
+        return uploads;
+    }
+
+    public string GetImageUrl(string key)
+    {
+        var client = ConnectClient();
+
+        key = Uri.UnescapeDataString(key);
+
+        var request = new GetPreSignedUrlRequest
+        {
+            BucketName = _bucketName,
+            Key = key,
+            Verb = HttpVerb.GET,
+            Expires = DateTime.UtcNow.AddMinutes(10)
+        };
+
+        var presignedUrl = client.GetPreSignedURL(request);
+
+        return presignedUrl;
+    }
+
+    public List<string> GetImageUrl(List<string> keys)
+    {
+        var client = ConnectClient();
+
+        var list = new List<string>();
+        foreach (var item in keys)
+        {
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = _bucketName,
+                Key = Uri.UnescapeDataString(item),
+                Verb = HttpVerb.GET,
+                Expires = DateTime.UtcNow.AddMinutes(10)
+            };
+            var presignedUrl = client.GetPreSignedURL(request);
+
+            list.Add(presignedUrl);
+        }
+
+        return list;
+    }
+
+    public async Task UploadAsync(string key, byte[] content, string contentType)
+    {
+        var client = ConnectClient();
+
+        using var stream = new MemoryStream(content);
+
+        var request = new PutObjectRequest
+        {
+            BucketName = _bucketName,
+            AutoCloseStream = true,
+            InputStream = stream,
+            ContentType = contentType,
+            Key = key
+        };
+
+        await client.PutObjectAsync(request);
+    }
+
+    private AmazonS3Client ConnectClient()
+    {
+        return new AmazonS3Client(_credentials, new AmazonS3Config
+        {
+            ServiceURL = _serviceURL,
+            ForcePathStyle = true,
+            AuthenticationRegion = "auto"
+        });
+    }
+}
