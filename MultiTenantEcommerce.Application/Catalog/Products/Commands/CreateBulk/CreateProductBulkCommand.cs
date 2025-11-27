62fb02dc6cfa3364bd69aaf0309f5682708f375e
david <didrijo@gmail.com>
2025-11-27 00:51:46 +0000

    chore: removed createProductBulk

--- MultiTenantEcommerce.Application/Catalog/Products/Commands/CreateBulk/CreateProductBulkCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using MultiTenantEcommerce.Application.Catalog.Products.Commands.Create;
-using MultiTenantEcommerce.Application.Catalog.Products.DTOs.Products;
-using MultiTenantEcommerce.Application.Common.Interfaces.CQRS;
-
-namespace MultiTenantEcommerce.Application.Catalog.Products.Commands.CreateBulk;
-public record CreateProductBulkCommand(
-    IEnumerable<CreateProductCommand> Products) : ICommand<List<ProductResponseAdminDTO>>;
