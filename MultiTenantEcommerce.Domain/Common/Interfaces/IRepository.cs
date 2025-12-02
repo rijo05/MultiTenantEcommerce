d96cf78d73e3a01ad86c19a84a743efbd7d625da
david <didrijo@gmail.com>
2025-12-02 23:39:05 +0000

    refactor: changes to getById

--- MultiTenantEcommerce.Domain/Common/Interfaces/IRepository.cs
+++ MultiTenantEcommerce.Domain/Common/Interfaces/IRepository.cs
@@ -6,9 +6,8 @@ namespace MultiTenantEcommerce.Domain.Common.Interfaces;
 public interface IRepository<T> where T : BaseEntity
 {
     public Task<List<T>> GetAllAsync();
-    public Task<T?> GetByIdAsync(params object[] keyValues);
+    public Task<T?> GetByIdAsync(Guid id);
     public Task<List<T>> GetByIdsAsync(IEnumerable<Guid> ids);
-    public Task<T?> GetByIdIncluding(Guid id, params Expression<Func<T, object>>[] includes);
     public Task AddAsync(T entity);
     public Task DeleteAsync(T entity);
     public Task UpdateAsync(T entity);
