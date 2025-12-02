1007f8baaf9e83b79e9aae337254d8d737a6b0b9
david <didrijo@gmail.com>
2025-12-02 23:33:57 +0000

    feat: add shipmentMapper and shipmentDTO

---
+++ MultiTenantEcommerce.Application/Shipping/DTOs/ShipmentDTO.cs
@@ -0,0 +1,26 @@
+﻿using MultiTenantEcommerce.Application.Common.DTOs.Address;
+using MultiTenantEcommerce.Domain.Shipping.Enums;
+using MultiTenantEcommerce.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantEcommerce.Application.Shipping.DTOs;
+public class ShipmentDTO
+{
+    public Guid OrderId { get; init; }
+    public string TrackingNumber { get; init; }
+    public AddressResponseDTO Address { get; init; }
+    public ShipmentCarrier Carrier { get; init; }
+    public ShipmentStatus Status { get; init; }
+    public string LabelKey { get; init; }
+    public Money Price { get; init; }
+
+    public DateTime? DeliveredAt { get; init; }
+    public DateTime? ShippedAt { get; init; }
+    public DateTime? EstimatedDeliveryDate { get; init; }
+    public TimeSpan MinTransit { get; init; }
+    public TimeSpan MaxTransit { get; init; }
+}
---
+++ MultiTenantEcommerce.Application/Shipping/Mappers/ShipmentMapper.cs
@@ -0,0 +1,38 @@
+﻿using MultiTenantEcommerce.Application.Common.Helpers.Mappers;
+using MultiTenantEcommerce.Application.Shipping.DTOs;
+using MultiTenantEcommerce.Domain.Shipping.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiTenantEcommerce.Application.Shipping.Mappers;
+public class ShipmentMapper
+{
+    private readonly AddressMapper _addressMapper;
+
+    public ShipmentMapper(AddressMapper addressMapper)
+    {
+        _addressMapper = addressMapper;
+    }
+
+    public ShipmentDTO ToShipmentDTO(Shipment shipment)
+    {
+        return new ShipmentDTO()
+        {
+            OrderId = shipment.OrderId,
+            Address = _addressMapper.ToAddressResponseFromDTO(shipment.Address),
+            Carrier = shipment.Carrier,
+            LabelKey = shipment.LabelKey,
+            TrackingNumber = shipment.TrackingNumber,
+            Status = shipment.Status,
+            Price = shipment.Price,
+            ShippedAt = shipment.ShippedAt,
+            EstimatedDeliveryDate = shipment.EstimatedDeliveryDate,
+            DeliveredAt = shipment.DeliveredAt,
+            MinTransit = shipment.MinTransit,
+            MaxTransit = shipment.MaxTransit    
+        };
+    }
+}
